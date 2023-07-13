e10061318210fc567356068f63b8f96277ead5a6
nlom0218 <nlom0218@naver.com>
2023-07-13 20:49:56 +0900

    feat: ThemeProvider 적용

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,12 @@
+import { ThemeProvider } from 'styled-components';
 import GlobalStyles from './styles/globalStyle';
+import { lightTheme } from './styles/theme';
 
 const App = () => {
   return (
     <>
       <GlobalStyles />
-      Welcome Haru Study!
+      <ThemeProvider theme={lightTheme}>Welcome Haru Study!</ThemeProvider>
     </>
   );
 };
