9f94d5d8a9bf47854457077b96b9cefd874b6a34
nlom0218 <nlom0218@naver.com>
2023-07-15 00:31:16 +0900

    refactor: theme 속성 변경

--- frontend/src/styles/theme.ts
+++ frontend/src/styles/theme.ts
@@ -1,57 +1,11 @@
 import color from './color';
 
 export const lightTheme = {
-  text: {
-    primary: color.black,
-    secondary: color.neutral[400],
-    accent: color.yellow,
-  },
-  background: {
-    basic: color.white,
-    primary: color.blue[500],
-    secondary: color.neutral[100],
-    studying: color.red[500],
-    retrospect: color.teal[500],
-  },
-  hoverBackground: {
-    basic: color.neutral[50],
-    primary: color.blue[600],
-    secondary: color.neutral[200],
-    studying: color.red[600],
-    retrospect: color.teal[600],
-  },
-  border: {
-    primary: color.blue[500],
-    secondary: color.neutral[200],
-    studying: color.red[500],
-    retrospect: color.teal[500],
-  },
+  background: color.black,
+  text: color.white,
 };
 
 export const darkTheme = {
-  text: {
-    primary: color.black,
-    secondary: color.neutral[400],
-    accent: color.yellow,
-  },
-  background: {
-    basic: color.black,
-    primary: color.blue[500],
-    secondary: color.neutral[800],
-    studying: color.red[500],
-    retrospect: color.teal[500],
-  },
-  hoverBackground: {
-    basic: color.neutral[50],
-    primary: color.blue[600],
-    secondary: color.neutral[200],
-    studying: color.red[600],
-    retrospect: color.teal[600],
-  },
-  border: {
-    primary: color.blue[500],
-    secondary: color.neutral[200],
-    studying: color.red[500],
-    retrospect: color.teal[500],
-  },
+  background: color.white,
+  text: color.black,
 };
--- frontend/src/types/styled-components.d.ts
+++ frontend/src/types/styled-components.d.ts
@@ -3,30 +3,7 @@ import { Variant } from './style';
 
 declare module 'styled-components' {
   export interface DefaultTheme {
-    text: {
-      primary: string;
-      secondary: string;
-      accent: string;
-    };
-    background: {
-      basic: string;
-      primary: string;
-      secondary: string;
-      studying: string;
-      retrospect: string;
-    };
-    hoverBackground: {
-      basic: string;
-      primary: string;
-      secondary: string;
-      studying: string;
-      retrospect: string;
-    };
-    border: {
-      primary: string;
-      secondary: string;
-      studying: string;
-      retrospect: string;
-    };
+    background: string;
+    text: string;
   }
 }
