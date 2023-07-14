7610637be560dc0768d47d149b9f04ad58f0d8ad
woo-jk <201702042@o.cnu.ac.kr>
2023-07-14 16:52:59 +0900

    feat: App 소개 문구 Typography 적용

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,5 @@
 import { Outlet } from 'react-router-dom';
-import { ThemeProvider, css } from 'styled-components';
+import { ThemeProvider } from 'styled-components';
 import GlobalStyles from './styles/globalStyle';
 import { lightTheme } from './styles/theme';
 import Typography from './components/common/Typography/Typography';
@@ -9,7 +9,7 @@ const App = () => {
     <>
       <GlobalStyles />
       <ThemeProvider theme={lightTheme}>
-        Welcome Haru Study!
+        <Typography variant="p1">Welcome Haru Study!</Typography>
         <Outlet />
       </ThemeProvider>
     </>
