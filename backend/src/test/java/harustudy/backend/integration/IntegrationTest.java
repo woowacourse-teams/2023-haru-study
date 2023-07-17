d3bcb6ff537ad9eb5a784912d3e0b8a17cbea5db
woosung1223 <dntjd991223@naver.com>
2023-07-17 21:46:36 +0900

    feat: 스터디 생성 기능 통합테스트 구현
    Co-authored-by: jaehee329 <jjhs9803@gmail.com>

---
+++ backend/src/test/java/harustudy/backend/integration/IntegrationTest.java
@@ -0,0 +1,30 @@
+package harustudy.backend.integration;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayNameGeneration;
+import org.junit.jupiter.api.DisplayNameGenerator.ReplaceUnderscores;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.context.WebApplicationContext;
+
+@SuppressWarnings("NonAsciiCharacters")
+@DisplayNameGeneration(ReplaceUnderscores.class)
+@SpringBootTest
+@AutoConfigureMockMvc
+@Transactional
+public class IntegrationTest {
+
+    @Autowired
+    WebApplicationContext webApplicationContext;
+
+    protected MockMvc mockMvc;
+
+    @BeforeEach
+    void setUp() {
+        this.mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
+    }
+}
