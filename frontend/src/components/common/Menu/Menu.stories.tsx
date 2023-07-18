6fbd39eb5e84f75cae0e94cc17592b9d2a8c5d15
nlom0218 <nlom0218@naver.com>
2023-07-18 21:12:19 +0900

    feat: Menu 스타일링

--- frontend/src/components/common/Menu/Menu.stories.tsx
+++ frontend/src/components/common/Menu/Menu.stories.tsx
@@ -21,7 +21,7 @@ export const DefaultMenu: Story = {
   args: {
     children: (
       <>
-        <Menu.Item onClick={() => alert('아이템1을 클릭했습니다.')}>아이템1</Menu.Item>
+        <Menu.Item onClick={() => alert('아이템1을 클릭했습니다.')}>아이템1입니다.</Menu.Item>
         <Menu.Item onClick={() => alert('아이템2을 클릭했습니다.')}>아이템2</Menu.Item>
         <Menu.Item onClick={() => alert('아이템3을 클릭했습니다.')}>아이템3</Menu.Item>
         <Menu.Item onClick={() => alert('아이템4을 클릭했습니다.')}>아이템4</Menu.Item>
--- frontend/src/components/common/Menu/Menu.tsx
+++ frontend/src/components/common/Menu/Menu.tsx
@@ -1,6 +1,8 @@
 import { LiHTMLAttributes, PropsWithChildren } from 'react';
 import { styled } from 'styled-components';
 
+import color from '@Styles/color';
+
 import HamburgerIcon from '@Assets/icons/HamburgerIcon';
 
 const Menu = ({ children }: PropsWithChildren) => {
@@ -14,14 +16,43 @@ const Menu = ({ children }: PropsWithChildren) => {
 
 export default Menu;
 
-const MenuLayout = styled.div``;
+const MenuLayout = styled.div`
+  position: relative;
 
-const MenuList = styled.ul``;
+  svg {
+    cursor: pointer;
+  }
+`;
+
+const MenuList = styled.ul`
+  position: absolute;
+  top: 30px;
+
+  display: grid;
+  row-gap: 6px;
+
+  width: fit-content;
+
+  padding: 10px 0px;
+  border-radius: 8px;
+
+  box-shadow: rgba(99, 99, 99, 0.2) 0px 2px 8px 0px;
+`;
 
 const MenuItem = ({ children, onClick }: PropsWithChildren<LiHTMLAttributes<HTMLLIElement>>) => {
   return <MenuItemLayout onClick={onClick}>{children}</MenuItemLayout>;
 };
 
-const MenuItemLayout = styled.li``;
+const MenuItemLayout = styled.li`
+  padding: 6px 24px;
+
+  transition: background-color 0.2s ease;
+
+  cursor: pointer;
+
+  &:hover {
+    background-color: ${color.neutral[100]};
+  }
+`;
 
 Menu.Item = MenuItem;
