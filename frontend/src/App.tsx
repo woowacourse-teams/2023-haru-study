fbcd643b26b53b383052aeb6bc048b5a9f4d53fc
nlom0218 <nlom0218@naver.com>
2023-07-19 11:19:20 +0900

    feat: closeMenu 함수 적용

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,6 +2,7 @@ import { Outlet } from 'react-router-dom';
 import { ThemeProvider } from 'styled-components';
 
 import Button from '@Components/common/Button/Button';
+import Menu from '@Components/common/Menu/Menu';
 import Typography from '@Components/common/Typography/Typography';
 
 import GlobalStyles from '@Styles/globalStyle';
@@ -14,6 +15,12 @@ const App = () => {
       <ThemeProvider theme={lightTheme}>
         <Typography variant="h1">Welcome Haru Study!</Typography>
         <Button variant="secondary">Button</Button>
+        <Menu>
+          <Menu.Item onClick={() => alert('아이템1을 클릭했습니다.')}>아이템1입니다.</Menu.Item>
+          <Menu.Item onClick={() => alert('아이템2을 클릭했습니다.')}>아이템2</Menu.Item>
+          <Menu.Item onClick={() => alert('아이템3을 클릭했습니다.')}>아이템3</Menu.Item>
+          <Menu.Item onClick={() => alert('아이템4을 클릭했습니다.')}>아이템4</Menu.Item>
+        </Menu>
         <Outlet />
       </ThemeProvider>
     </>
--- frontend/src/components/common/Menu/Menu.tsx
+++ frontend/src/components/common/Menu/Menu.tsx
@@ -1,4 +1,13 @@
-import { LiHTMLAttributes, PropsWithChildren } from 'react';
+import React, {
+  Children,
+  LiHTMLAttributes,
+  PropsWithChildren,
+  ReactElement,
+  cloneElement,
+  useEffect,
+  useRef,
+  useState,
+} from 'react';
 import { styled } from 'styled-components';
 
 import color from '@Styles/color';
@@ -6,10 +15,39 @@ import color from '@Styles/color';
 import HamburgerIcon from '@Assets/icons/HamburgerIcon';
 
 const Menu = ({ children }: PropsWithChildren) => {
+  const [isOpenMenu, setIsOpenMenu] = useState(false);
+  const ref = useRef<HTMLDivElement>(null);
+
+  const toggleMenu = () => setIsOpenMenu((prev) => !prev);
+
+  const closeMenu = () => setIsOpenMenu(false);
+
+  useEffect(() => {
+    const handleClickOutside = (e: MouseEvent) => {
+      if (ref.current && !ref.current.contains(e.target as Node)) {
+        closeMenu();
+      }
+    };
+
+    document.addEventListener('mousedown', handleClickOutside);
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside);
+    };
+  }, [ref]);
+
   return (
-    <MenuLayout>
-      <HamburgerIcon />
-      <MenuList>{children}</MenuList>
+    <MenuLayout ref={ref}>
+      <MenuIconWrapper onClick={toggleMenu}>
+        <HamburgerIcon />
+      </MenuIconWrapper>
+      {isOpenMenu && (
+        <MenuList>
+          {Children.map(children, (child) => {
+            const item = child as ReactElement;
+            return cloneElement(item, { closeMenu });
+          })}
+        </MenuList>
+      )}
     </MenuLayout>
   );
 };
@@ -19,10 +57,14 @@ export default Menu;
 const MenuLayout = styled.div`
   position: relative;
 
+  width: fit-content;
+
   svg {
     cursor: pointer;
   }
 `;
+
+const MenuIconWrapper = styled.div``;
 
 const MenuList = styled.ul`
   position: absolute;
@@ -31,7 +73,7 @@ const MenuList = styled.ul`
   display: grid;
   row-gap: 6px;
 
-  width: fit-content;
+  width: max-content;
 
   padding: 10px 0px;
   border-radius: 8px;
@@ -39,8 +81,20 @@ const MenuList = styled.ul`
   box-shadow: rgba(99, 99, 99, 0.2) 0px 2px 8px 0px;
 `;
 
-const MenuItem = ({ children, onClick }: PropsWithChildren<LiHTMLAttributes<HTMLLIElement>>) => {
-  return <MenuItemLayout onClick={onClick}>{children}</MenuItemLayout>;
+type MenuItemProps = PropsWithChildren<{ closeMenu?: () => void; onClick: () => void }> &
+  LiHTMLAttributes<HTMLLIElement>;
+
+const MenuItem = ({ children, onClick, closeMenu, ...props }: MenuItemProps) => {
+  const handleClick = () => {
+    if (closeMenu) closeMenu();
+    onClick();
+  };
+
+  return (
+    <MenuItemLayout onClick={handleClick} {...props}>
+      {children}
+    </MenuItemLayout>
+  );
 };
 
 const MenuItemLayout = styled.li`
