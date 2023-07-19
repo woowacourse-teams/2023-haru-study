137579d357acaa34c5bd4bf15207f4b0d5c5366e
nlom0218 <nlom0218@naver.com>
2023-07-19 22:12:52 +0900

    feat: jpg, png, jpeg, gif 타입 및 웹팩 설정

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -47,6 +47,10 @@ module.exports = () => {
           },
           exclude: /node_modules/,
         },
+        {
+          test: /\.(png|svg|jpg|jpeg|gif)$/i,
+          type: 'asset/resource',
+        },
       ],
     },
     plugins: [
