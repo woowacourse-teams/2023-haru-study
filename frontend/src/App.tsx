6febf08cb04b576978d34ad7620169a0fbf62b51
yeopto <gunyeop530@gmail.com>
2023-07-20 02:02:47 +0900

    chore: App.tsx에 Typography 및 Button 삭제

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,6 @@
 import { Outlet } from 'react-router-dom';
 import { ThemeProvider } from 'styled-components';
 
-import Button from '@Components/common/Button/Button';
-import Typography from '@Components/common/Typography/Typography';
-
 import GlobalStyles from '@Styles/globalStyle';
 import { lightTheme } from '@Styles/theme';
 
@@ -12,8 +9,6 @@ const App = () => {
     <>
       <GlobalStyles />
       <ThemeProvider theme={lightTheme}>
-        <Typography variant="h1">Welcome Haru Study!</Typography>
-        <Button variant="secondary">Button</Button>
         <Outlet />
       </ThemeProvider>
     </>
