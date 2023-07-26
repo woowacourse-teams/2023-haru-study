17817f223cdfa97dfc936b58e566b3d5bc82f49a
woo-jk <201702042@o.cnu.ac.kr>
2023-07-26 13:17:14 +0900

    refactor: usePlanningForm 구현 및 적용

--- frontend/src/components/board/PlanningForm/PlanningForm.tsx
+++ frontend/src/components/board/PlanningForm/PlanningForm.tsx
@@ -1,10 +1,9 @@
-import { useState } from 'react';
 import { styled } from 'styled-components';
 
 import Button from '@Components/common/Button/Button';
 import QuestionTextarea from '@Components/common/QuestionTextarea/QuestionTextarea';
 
-import useQuestionTextarea from '@Hooks/useQuestionTextarea';
+import usePlanningForm from '@Hooks/board/usePlanningForm';
 
 type Props = {
   cycle: number;
@@ -13,79 +12,43 @@ type Props = {
 };
 
 const PlanningForm = ({ cycle, minutes, onClickSubmitButton }: Props) => {
-  const [isSubmitLoading, setIsSubmitLoading] = useState(false);
-
-  const toDoProps = useQuestionTextarea({
-    question: `${cycle}번째 사이클(${minutes}분)에서 학습할 것은 무엇인가요?`,
-    minLength: 10,
-    maxLength: 500,
-    required: true,
-  });
-
-  const completionConditionProps = useQuestionTextarea({
-    question: `그 학습의 완료 조건은 무엇인가요?`,
-    minLength: 10,
-    maxLength: 500,
-    required: true,
-  });
-
-  const expectedProbabilityProps = useQuestionTextarea({
-    question: `학습을 ${minutes}분만에 성공적으로 마칠 확률은 몇 %로 예상되나요? 그 이유는 무엇인가요?`,
-    maxLength: 500,
-  });
-
-  const expectedDifficultyProps = useQuestionTextarea({
-    question: `${minutes}분의 시간 동안 가장 큰 어려움으로 예상되는 것은 무엇인가요?`,
-    maxLength: 500,
-  });
-
-  const whatCanYouDoProps = useQuestionTextarea({
-    question: `성공 확률이 80% 미만이라면, 80% 이상으로 만들기 위해 무엇을 할 수 있나요?`,
-    maxLength: 500,
-  });
-
-  const isDisabledButton = !!(
-    toDoProps.errorMessage ||
-    completionConditionProps.errorMessage ||
-    expectedDifficultyProps.errorMessage ||
-    expectedProbabilityProps.errorMessage ||
-    whatCanYouDoProps.errorMessage
-  );
+  const { questionTextareaProps, isInvalidForm, isSubmitLoading, submitForm } = usePlanningForm();
 
   const handleClickButton = async () => {
-    setIsSubmitLoading(true);
-    const response = await fetch('/api/studies/123/members/1/content/plans', {
-      method: 'POST',
-      body: JSON.stringify({
-        toDo: toDoProps.value,
-        completionCondition: completionConditionProps.value,
-        expectedProbability: expectedProbabilityProps.value,
-        expectedDifficulty: expectedDifficultyProps.value,
-        whatCanYouDo: whatCanYouDoProps.value,
-      }),
-    });
-
-    setIsSubmitLoading(false);
-    if (!response.ok) {
-      alert('에러 발생');
-      return;
+    try {
+      await submitForm();
+      onClickSubmitButton();
+    } catch (error) {
+      if (!(error instanceof Error)) return;
+      alert(error.message);
     }
-    onClickSubmitButton();
   };
 
   return (
     <Layout>
-      <QuestionTextarea {...toDoProps} />
-      <QuestionTextarea {...completionConditionProps} />
-      <QuestionTextarea {...expectedProbabilityProps} />
-      <QuestionTextarea {...expectedDifficultyProps} />
-      <QuestionTextarea {...whatCanYouDoProps} />
+      <QuestionTextarea
+        question={`${cycle}번째 사이클(${minutes}분)에서 학습할 것은 무엇인가요?`}
+        {...questionTextareaProps.toDo}
+      />
+      <QuestionTextarea question={`그 학습의 완료 조건은 무엇인가요?`} {...questionTextareaProps.completionCondition} />
+      <QuestionTextarea
+        question={`학습을 ${minutes}분만에 성공적으로 마칠 확률은 몇 %로 예상되나요? 그 이유는 무엇인가요?`}
+        {...questionTextareaProps.expectedProbability}
+      />
+      <QuestionTextarea
+        question={`${minutes}분의 시간 동안 가장 큰 어려움으로 예상되는 것은 무엇인가요?`}
+        {...questionTextareaProps.expectedDifficulty}
+      />
+      <QuestionTextarea
+        question={`성공 확률이 80% 미만이라면, 80% 이상으로 만들기 위해 무엇을 할 수 있나요?`}
+        {...questionTextareaProps.whatCanYouDo}
+      />
       <Button
         variant="primary"
         type="submit"
         onClick={handleClickButton}
         isLoading={isSubmitLoading}
-        disabled={isDisabledButton}
+        disabled={isInvalidForm}
       >
         학습 시작하기
       </Button>
---
+++ frontend/src/hooks/board/usePlanningForm.ts
@@ -0,0 +1,61 @@
+import { useState } from 'react';
+
+import useQuestionTextarea from '@Hooks/useQuestionTextarea';
+
+const usePlanningForm = () => {
+  const [isSubmitLoading, setIsSubmitLoading] = useState(false);
+
+  const questionTextareaProps = {
+    toDo: useQuestionTextarea({
+      minLength: 10,
+      maxLength: 500,
+      required: true,
+    }),
+    completionCondition: useQuestionTextarea({
+      minLength: 10,
+      maxLength: 500,
+      required: true,
+    }),
+    expectedProbability: useQuestionTextarea({
+      maxLength: 500,
+    }),
+    expectedDifficulty: useQuestionTextarea({
+      maxLength: 500,
+    }),
+    whatCanYouDo: useQuestionTextarea({
+      maxLength: 500,
+    }),
+  } as const;
+
+  const isInvalidForm = !!(
+    questionTextareaProps.toDo.errorMessage ||
+    questionTextareaProps.completionCondition.errorMessage ||
+    questionTextareaProps.expectedDifficulty.errorMessage ||
+    questionTextareaProps.expectedProbability.errorMessage ||
+    questionTextareaProps.whatCanYouDo.errorMessage
+  );
+
+  const submitForm = async () => {
+    setIsSubmitLoading(true);
+    const response = await fetch('/api/studies/123/members/1/content/plans', {
+      method: 'POST',
+      body: JSON.stringify({
+        toDo: questionTextareaProps.toDo.value,
+        completionCondition: questionTextareaProps.completionCondition.value,
+        expectedProbability: questionTextareaProps.expectedProbability.value,
+        expectedDifficulty: questionTextareaProps.expectedDifficulty.value,
+        whatCanYouDo: questionTextareaProps.whatCanYouDo.value,
+      }),
+    });
+
+    setIsSubmitLoading(false);
+
+    if (!response.ok) {
+      throw new Error('제출 과정에 에러가 발생했습니다. 다시 시도 해주세요.');
+    }
+  };
+
+  return { questionTextareaProps, isSubmitLoading, isInvalidForm, submitForm };
+};
+
+export default usePlanningForm;
