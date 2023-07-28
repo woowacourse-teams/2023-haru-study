1d126f6b5075151a484d20140532379409d31813
MODI <77962265+jaehee329@users.noreply.github.com>
2023-07-28 12:11:35 +0900

    [BE] 스터디 최대 진행 시간 40분에서 60분으로 변경 (#202)
    Co-authored-by: MoonJeWoong <munjin0201@naver.com>
    Co-authored-by: aak2075 <lks971003@gmail.com>
    Co-authored-by: woosung1223 <dntjd991223@naver.com>

--- backend/src/main/java/harustudy/backend/room/dto/CreatePomodoroRoomRequest.java
+++ backend/src/main/java/harustudy/backend/room/dto/CreatePomodoroRoomRequest.java
@@ -1,14 +1,12 @@
 package harustudy.backend.room.dto;
 
-import jakarta.validation.constraints.Max;
-import jakarta.validation.constraints.Min;
-import jakarta.validation.constraints.Size;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 
 public record CreatePomodoroRoomRequest(
-        @Size(min = 1, max = 10) String name,
-        @Min(1) @Max(8) Integer totalCycle,
-        @Min(20) @Max(40) Integer timePerCycle) {
+        @NotBlank String name,
+        @NotNull Integer totalCycle,
+        @NotNull Integer timePerCycle
+) {
 
-    public CreatePomodoroRoomRequest {
-    }
 }
--- backend/src/test/java/harustudy/backend/room/domain/PomodoroRoomTest.java
+++ backend/src/test/java/harustudy/backend/room/domain/PomodoroRoomTest.java
@@ -68,16 +68,16 @@ class PomodoroRoomTest {
     }
 
     @ParameterizedTest
-    @ValueSource(ints = {20, 40})
-    void 스터디_시간은_최소_20분_최대_40분이_정상_케이스이다(int timePerCycle) {
+    @ValueSource(ints = {20, 60})
+    void 스터디_시간은_최소_20분_최대_60분이_정상_케이스이다(int timePerCycle) {
         // given, when, then
         assertThatCode(() -> new PomodoroRoom("teo", 5, timePerCycle, participantCode))
                 .doesNotThrowAnyException();
     }
 
     @ParameterizedTest
-    @ValueSource(ints = {19, 41})
-    void 스터디_시간은_20분_미만이거나_40분_초과라면_예외를_던진다(int timePerCycle) {
+    @ValueSource(ints = {19, 61})
+    void 스터디_시간은_20분_미만이거나_60분_초과라면_예외를_던진다(int timePerCycle) {
         // given, when, then
         assertThatThrownBy(() -> new PomodoroRoom("teo", 5, timePerCycle, participantCode))
                 .isInstanceOf(PomodoroTimePerCycleException.class);
