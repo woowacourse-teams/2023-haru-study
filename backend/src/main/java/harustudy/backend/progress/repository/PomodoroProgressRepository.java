275d6dc9e4c04c91d1ea48b00a915d89091586b6
MoonJeWoong <munjin0201@naver.com>
2023-08-02 10:18:26 +0900

    fix: 페치 조인문에 누락된 조건 추가
    Co-authored-by: aak2075 <lks971003@gmail.com>
    Co-authored-by: woosung1223 <dntjd991223@naver.com>
    Co-authored-by: jaehee329 <jjhs9803@gmail.com>

--- backend/src/main/java/harustudy/backend/progress/repository/PomodoroProgressRepository.java
+++ backend/src/main/java/harustudy/backend/progress/repository/PomodoroProgressRepository.java
@@ -7,13 +7,16 @@ import java.util.List;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface PomodoroProgressRepository extends JpaRepository<PomodoroProgress, Long> {
 
-    Optional<PomodoroProgress> findByPomodoroRoomAndMember(PomodoroRoom pomodoroRoom, Member member);
+    Optional<PomodoroProgress> findByPomodoroRoomAndMember(PomodoroRoom pomodoroRoom,
+            Member member);
 
     List<PomodoroProgress> findAllByPomodoroRoom(PomodoroRoom pomodoroRoom);
 
-    @Query("select p from PomodoroProgress p join fetch p.member")
-    List<PomodoroProgress> findAllByPomodoroRoomFetchMember(PomodoroRoom pomodoroRoom);
+    @Query("select p from PomodoroProgress p join fetch p.member where p.pomodoroRoom = :pomodoroRoom")
+    List<PomodoroProgress> findAllByPomodoroRoomFetchMember(
+            @Param("pomodoroRoom") PomodoroRoom pomodoroRoom);
 }
