9368846c1c5fbead0bcb0194e26085446bdbefc7
nlom0218 <nlom0218@naver.com>
2023-08-02 17:44:04 +0900

    refactor: StudyParticipant 컴포넌트 StudyParticipation으로 네이밍 변경
    Co-authored-by: Jungkyun Woo <201702042@o.cnu.ac.kr>
    Co-authored-by: Geonyeop Kim <gunyeop530@gmail.com>

--- frontend/src/pages/StudyParticipation.tsx
+++ frontend/src/pages/StudyParticipation.tsx
@@ -1,12 +1,12 @@
 import ParticipationCodeInput from '@Components/preparation/ParticipationCodeInput/ParticipationCodeInput';
 
-import StudyPrepationLayout from './layout/StudyPreparationLayout';
+import StudyPreparationLayout from './layout/StudyPreparationLayout';
 
 const StudyParticipation = () => {
   return (
-    <StudyPrepationLayout headerText={`스터디 참여하기`}>
+    <StudyPreparationLayout headerText={`스터디 참여하기`}>
       <ParticipationCodeInput />
-    </StudyPrepationLayout>
+    </StudyPreparationLayout>
   );
 };
 
