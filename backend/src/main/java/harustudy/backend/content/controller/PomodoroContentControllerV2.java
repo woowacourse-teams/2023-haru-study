e8446eb622f971bd7c0ff50a308aad956cee0c2c
woosung1223 <dntjd991223@naver.com>
2023-08-02 15:39:12 +0900

    chore: 와일드카드 import문 제거

--- backend/src/main/java/harustudy/backend/content/controller/PomodoroContentControllerV2.java
+++ backend/src/main/java/harustudy/backend/content/controller/PomodoroContentControllerV2.java
@@ -6,7 +6,12 @@ import harustudy.backend.content.dto.WriteRetrospectRequest;
 import harustudy.backend.content.service.PomodoroContentServiceV2;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @RequiredArgsConstructor
 @RestController
