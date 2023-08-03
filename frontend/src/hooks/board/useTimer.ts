4c3f53ccc940461a5a4b085857f1c33b3374e1c6
woo-jk <201702042@o.cnu.ac.kr>
2023-08-03 20:09:03 +0900

    fix: 브라우저 탭 비활성 시 타이머 느리게 흘러가는 문제 해결

--- frontend/src/hooks/board/useTimer.ts
+++ frontend/src/hooks/board/useTimer.ts
@@ -7,9 +7,10 @@ const SECONDS_PER_MINUTE = 60;
 
 const useTimer = (studyMinutes: number, step: Step) => {
   const timerId = useRef<NodeJS.Timer | null>(null);
+  const startTime = useRef(Date.now());
+  const initTime = useRef(PLANNING_MINUTES * SECONDS_PER_MINUTE);
 
-  const initialMinutes = PLANNING_MINUTES * SECONDS_PER_MINUTE;
-  const [leftTime, setLeftTime] = useState(initialMinutes);
+  const [leftTime, setLeftTime] = useState(initTime.current);
   const [isTicking, setIsTicking] = useState(false);
 
   const clear = () => {
@@ -21,7 +22,9 @@ const useTimer = (studyMinutes: number, step: Step) => {
 
   const tick = useCallback(() => {
     if (leftTime > 0) {
-      setLeftTime(leftTime - 1);
+      const timeDifference = Math.floor((Date.now() - startTime.current) / 1000);
+
+      setLeftTime(initTime.current - timeDifference);
     }
     if (leftTime <= 1) {
       setIsTicking(false);
@@ -41,18 +44,22 @@ const useTimer = (studyMinutes: number, step: Step) => {
     setIsTicking(false);
 
     if (step === 'planning' || step === 'retrospect') {
+      initTime.current = PLANNING_MINUTES * SECONDS_PER_MINUTE;
       setLeftTime(PLANNING_MINUTES * SECONDS_PER_MINUTE);
       return;
     }
 
+    initTime.current = studyMinutes * SECONDS_PER_MINUTE;
     setLeftTime(studyMinutes * SECONDS_PER_MINUTE);
   }, [studyMinutes, step]);
 
   const start = () => {
+    startTime.current = Date.now();
     setIsTicking(true);
   };
 
   const stop = () => {
+    initTime.current = leftTime;
     setIsTicking(false);
   };
 
