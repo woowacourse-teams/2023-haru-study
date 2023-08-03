0eac769c121b4f113e8e56c8b8425a6b6278e58c
nlom0218 <nlom0218@naver.com>
2023-08-04 00:35:08 +0900

    fix: StudyPrepartaion 분기 수정

--- frontend/src/pages/StudyPreparation.tsx
+++ frontend/src/pages/StudyPreparation.tsx
@@ -28,7 +28,7 @@ const StudyPreparation = () => {
 
   const memberId = getCookie('memberId');
 
-  const restart = () => setNickname(null);
+  const restart = () => setNickname('');
 
   const {
     state: { participantCode, studyName, isHost },
@@ -39,18 +39,28 @@ const StudyPreparation = () => {
 
     const { nickname } = await requestCheckIsMember(studyId, memberId);
 
-    setNickname(nickname);
+    // 서버에서 받은 nickname이 null인 경우 클라이언트 nickname 상태를 ""로 변경
+    if (!nickname) setNickname('');
+    // 서버에서 받은 nickname이 null인 경우 클라이언트 nickname 상태를 서버에서 받은 nickname으로 변경
+    else setNickname(nickname);
   }, [memberId, studyId]);
 
   useEffect(() => {
-    if (isHost) return;
+    if (isHost || !memberId) {
+      // 방장이 스터디를 개설한 뒤 또는 저장된 memberId가 없는 경우
+      // 닉네임이 없으니 nickname 상태를 ""로 변경
+      // 후 리턴
+      console.log(memberId);
+      setNickname('');
+      return;
+    }
     try {
       authenticateMember();
     } catch (error) {
       if (!(error instanceof Error)) throw error;
       alert(error.message);
     }
-  }, [authenticateMember, isHost]);
+  }, [authenticateMember, isHost, memberId]);
 
   if (!studyId) return <div>잘못된 접근입니다.</div>;
 
