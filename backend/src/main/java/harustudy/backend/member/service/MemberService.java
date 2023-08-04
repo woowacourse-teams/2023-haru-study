12fd2a27ba6ae77cc378ee95548bf3370ecb3b56
aak2075 <lks971003@gmail.com>
2023-08-04 10:16:58 +0900

    fix: memberId가 이미 있지만 다른 스터디를 참여할 때 null을 반환하도록 수정

--- backend/src/main/java/harustudy/backend/member/service/MemberService.java
+++ backend/src/main/java/harustudy/backend/member/service/MemberService.java
@@ -4,9 +4,7 @@ import harustudy.backend.common.EntityNotFoundException.MemberNotFound;
 import harustudy.backend.common.EntityNotFoundException.RoomNotFound;
 import harustudy.backend.member.domain.Member;
 import harustudy.backend.member.dto.NicknameResponse;
-import harustudy.backend.member.exception.MemberNotParticipatedException;
 import harustudy.backend.member.repository.MemberRepository;
-import harustudy.backend.progress.repository.PomodoroProgressRepository;
 import harustudy.backend.room.domain.PomodoroRoom;
 import harustudy.backend.room.repository.PomodoroRoomRepository;
 import lombok.RequiredArgsConstructor;
@@ -21,7 +19,6 @@ public class MemberService {
 
     private final PomodoroRoomRepository pomodoroRoomRepository;
     private final MemberRepository memberRepository;
-    private final PomodoroProgressRepository pomodoroProgressRepository;
 
     public NicknameResponse findParticipatedMemberNickname(Long roomId, Long memberId) {
         PomodoroRoom pomodoroRoom = pomodoroRoomRepository.findById(roomId)
@@ -29,9 +26,9 @@ public class MemberService {
         Member member = memberRepository.findById(memberId)
                 .orElseThrow(MemberNotFound::new);
 
-        pomodoroProgressRepository.findByPomodoroRoomAndMember(pomodoroRoom, member)
-                .orElseThrow(MemberNotParticipatedException::new);
-
-        return new NicknameResponse(member.getNickname());
+        if (pomodoroRoom.isParticipatedMember(member)) {
+            return new NicknameResponse(member.getNickname());
+        }
+        return new NicknameResponse(null);
     }
 }
