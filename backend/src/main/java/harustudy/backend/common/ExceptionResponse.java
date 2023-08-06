250d41bdcb8278dd81832ca4cb39344d83e0f71d
woosung1223 <dntjd991223@naver.com>
2023-08-06 19:26:33 +0900

    feat: code 필드가 null이라면 아예 전송하지 않도록 변경

--- backend/src/main/java/harustudy/backend/common/ExceptionResponse.java
+++ backend/src/main/java/harustudy/backend/common/ExceptionResponse.java
@@ -1,5 +1,8 @@
 package harustudy.backend.common;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public record ExceptionResponse(Integer code, String message) {
 
     public static ExceptionResponse from(ExceptionSituation exceptionSituation) {
