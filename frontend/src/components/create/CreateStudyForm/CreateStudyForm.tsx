63d63f52a51e431615b62e521ea06920611e8596
yeopto <gunyeop530@gmail.com>
2023-08-06 13:39:30 +0900

    refactor: 매직넘버 상수화

--- frontend/src/components/create/CreateStudyForm/CreateStudyForm.tsx
+++ frontend/src/components/create/CreateStudyForm/CreateStudyForm.tsx
@@ -18,6 +18,9 @@ import { STUDY_TIME_PER_CYCLE_OPTIONS, TOTAL_CYCLE_OPTIONS } from '@Constants/st
 
 import type { StudyTimePerCycleOptions, TotalCycleOptions } from '@Types/study';
 
+const ADDITIONAL_TIME = 20 as const;
+const DIVIDE_TIME = 60 as const;
+
 const CreateStudyForm = () => {
   const navigate = useNavigate();
 
@@ -31,9 +34,9 @@ const CreateStudyForm = () => {
 
   const { isLoading, createStudy } = useCreateStudy(errorHandler);
 
-  const totalTime = (Number(timePerCycleSelect.state ?? 0) + 20) * Number(totalCycleSelect.state ?? 0);
-  const hour = Math.floor(totalTime / 60);
-  const minute = totalTime % 60;
+  const totalTime = (Number(timePerCycleSelect.state ?? 0) + ADDITIONAL_TIME) * Number(totalCycleSelect.state ?? 0);
+  const hour = Math.floor(totalTime / DIVIDE_TIME);
+  const minute = totalTime % DIVIDE_TIME;
 
   const handleOnClickMakeButton = async () => {
     if (!totalCycleSelect.state || !timePerCycleSelect.state || !studyNameInput.state) {
