66abdcf2d457519653eae5ac1f56881c973573d9
noah <57981252+nlom0218@users.noreply.github.com>
2023-08-08 17:25:26 +0900

    [FE] 스터디 기록 페이지 API 훅 분리 및 v2 적용 (#265)
    * refactor: useStudyRecord 훅 분리
    
    * refactor: useMemberRecord 훅 분리
    
    * refactor: errorHandelr 함수 매개변수 변경
    
    * refactor: 스터디 기록 페이지 api v2으로 변경
    
    * feat: api BASE URL 분리
    
    * refactor: fetch 함수면 수정

---
+++ frontend/src/hooks/record/useMemberRecord.ts
@@ -0,0 +1,32 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+import { useCallback, useEffect, useState } from 'react';
+
+import { requestGetMemberRecordContents } from '@Apis/index';
+
+import type { MemberRecordContent } from '@Types/study';
+
+const useMemberRecord = (studyId: string, memberId: string, options?: { errorHandler: (error: Error) => void }) => {
+  const [memberRecordContents, setMemberRecordContents] = useState<MemberRecordContent[] | null>(null);
+
+  const isLoading = !memberRecordContents;
+
+  const fetchMemberRecordData = useCallback(async () => {
+    try {
+      const { content } = await requestGetMemberRecordContents(studyId, memberId);
+
+      setMemberRecordContents(content);
+    } catch (error) {
+      if (!(error instanceof Error)) throw error;
+
+      options?.errorHandler(error);
+    }
+  }, [memberId, studyId]);
+
+  useEffect(() => {
+    fetchMemberRecordData();
+  }, [fetchMemberRecordData]);
+
+  return { memberRecordContents, isLoading };
+};
+
+export default useMemberRecord;
