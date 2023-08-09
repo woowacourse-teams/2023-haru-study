68d8ba192383a16d17b17cb92a8b3a613e53a91a
MoonJeWoong <munjin0201@naver.com>
2023-08-09 12:41:41 +0900

    feat: WARN, ERROR 로그 분리
    Co-authored-by: jaehee329 <jjhs9803@gmail.com>

--- backend/src/main/java/harustudy/backend/common/ExceptionAdvice.java
+++ backend/src/main/java/harustudy/backend/common/ExceptionAdvice.java
@@ -1,21 +1,25 @@
 package harustudy.backend.common;
 
 import harustudy.backend.member.exception.MemberNotParticipatedException;
-import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-@Slf4j
 @RestControllerAdvice
 public class ExceptionAdvice {
+
+    Logger defaultLog = LoggerFactory.getLogger(ExceptionAdvice.class);
+    Logger exceptionLog = LoggerFactory.getLogger("ExceptionLogger");
 
     @ExceptionHandler(MemberNotParticipatedException.class)
     public ResponseEntity<ExceptionResponse> handleMemberNotParticipatedException(
             MemberNotParticipatedException e) {
-        log.info(e.getMessage());
+        defaultLog.error(e.getMessage());
+        exceptionLog.error(e.getMessage(), e);
         return ResponseEntity.status(HttpStatus.NOT_FOUND)
                 .body(new ExceptionResponse(MemberNotParticipatedException.CODE,
                         MemberNotParticipatedException.MESSAGE));
@@ -24,14 +28,16 @@ public class ExceptionAdvice {
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<ExceptionResponse> handleBindException(
             MethodArgumentNotValidException e) {
-        log.info(e.getMessage());
+        defaultLog.error(e.getMessage());
+        exceptionLog.error(e.getMessage(), e);
         return ResponseEntity.badRequest()
                 .body(new ExceptionResponse(null, e.getMessage()));
     }
 
     @ExceptionHandler(Exception.class)
     public ResponseEntity<Void> handleException(Exception e) {
-        log.info(e.getMessage());
+        defaultLog.error(e.getMessage());
+        exceptionLog.error(e.getMessage(), e);
         return ResponseEntity.internalServerError().build();
     }
 }
