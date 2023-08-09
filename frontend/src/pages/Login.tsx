7f406e2c6e0949eddcb08b33307947776e76f395
nlom0218 <nlom0218@naver.com>
2023-08-09 15:37:38 +0900

    feat: 로그인 페이지 레이아웃
    Co-authored-by: Jungkyun Woo <201702042@o.cnu.ac.kr>
    Co-authored-by: Geonyeop Kim <gunyeop530@gmail.com>

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -1,5 +1,53 @@
+import { css, styled } from 'styled-components';
+
+import Button from '@Components/common/Button/Button';
+import Typography from '@Components/common/Typography/Typography';
+
+import color from '@Styles/color';
+
 const Login = () => {
-  return <div>로그인</div>;
+  return (
+    <Layout>
+      <Typography
+        variant="h1"
+        $style={css`
+          display: inline;
+
+          font-size: 6rem;
+          font-weight: 200;
+        `}
+      >
+        <Emphasis>하루</Emphasis>스터디
+      </Typography>
+      <ButtonContainer>
+        <Button variant="primary">로그인</Button>
+        <Button variant="outlined">비회원으로 로그인</Button>
+      </ButtonContainer>
+    </Layout>
+  );
 };
 
 export default Login;
+
+const Layout = styled.div`
+  width: 100vw;
+  height: 100vh;
+
+  display: flex;
+  flex-direction: column;
+  gap: 40px;
+  justify-content: center;
+  align-items: center;
+`;
+
+const ButtonContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+
+  width: 360px;
+`;
+
+const Emphasis = styled.span`
+  color: ${color.blue[500]};
+`;
