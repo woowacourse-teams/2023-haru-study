724003b0b2ccd680d6f7b2ad473ad42110bb99fd
hiiro <31722737+MoonJeWoong@users.noreply.github.com>
2023-08-09 16:01:22 +0900

    [BE] Logback 설정
    * feat: Logback 설정
    
    Co-authored-by: jaehee329 <jjhs9803@gmail.com>
    
    * feat: WARN, ERROR 로그 분리
    
    Co-authored-by: jaehee329 <jjhs9803@gmail.com>
    
    * feat: 피드백 사항 반영
    
    Co-authored-by: jaehee329 <jjhs9803@gmail.com>
    
    * feat: 최신 로그 파일 관리 포인트 고정
    
    Co-authored-by: jaehee329 <jjhs9803@gmail.com>
    
    * refactor: 로그 파일 이름 형식 수정
    
    Co-authored-by: jaehee329 <jjhs9803@gmail.com>
    
    * fix: ExceptionAdvice 관련 충돌 해결
    
    ---------
    
    Co-authored-by: jaehee329 <jjhs9803@gmail.com>

---
+++ backend/src/main/java/harustudy/backend/common/CachingFilter.java
@@ -0,0 +1,28 @@
+package harustudy.backend.common;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+import org.springframework.web.util.ContentCachingRequestWrapper;
+import org.springframework.web.util.ContentCachingResponseWrapper;
+
+@Component
+public class CachingFilter extends OncePerRequestFilter {
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request,
+            HttpServletResponse response, FilterChain chain)
+            throws ServletException, IOException {
+
+        ContentCachingRequestWrapper wrappingRequest = new ContentCachingRequestWrapper(request);
+        ContentCachingResponseWrapper wrappingResponse = new ContentCachingResponseWrapper(response);
+
+        chain.doFilter(wrappingRequest, wrappingResponse);
+
+        wrappingResponse.copyBodyToResponse();
+    }
+}
--- backend/src/main/java/harustudy/backend/common/ExceptionAdvice.java
+++ backend/src/main/java/harustudy/backend/common/ExceptionAdvice.java
@@ -1,25 +1,31 @@
 package harustudy.backend.common;
 
-import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-@Slf4j
 @RestControllerAdvice
 public class ExceptionAdvice {
 
+    Logger defaultLog = LoggerFactory.getLogger(ExceptionAdvice.class);
+    Logger exceptionLog = LoggerFactory.getLogger("ExceptionLogger");
+
     @ExceptionHandler(HaruStudyException.class)
-    public ResponseEntity<ExceptionResponse> handleHaruStudyException(HaruStudyException e) {
+    public ResponseEntity<ExceptionResponse> handleMemberNotParticipatedException(
+            HaruStudyException e) {
         ExceptionSituation exceptionSituation = ExceptionMapper.getSituationOf(e);
-        log.info(exceptionSituation.getMessage());
+        defaultLog.error(exceptionSituation.getMessage());
+        exceptionLog.error(exceptionSituation.getMessage(), e);
         return ResponseEntity.status(exceptionSituation.getStatusCode())
                 .body(ExceptionResponse.from(exceptionSituation));
     }
 
     @ExceptionHandler(Exception.class)
     public ResponseEntity<Void> handleException(Exception e) {
-        log.error(e.getMessage());
+        defaultLog.error(e.getMessage());
+        exceptionLog.error(e.getMessage(), e);
         return ResponseEntity.internalServerError().build();
     }
 }
