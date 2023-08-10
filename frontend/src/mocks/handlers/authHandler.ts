e5e04459490d19189912e9c8431a7460f26bf00e
nlom0218 <nlom0218@naver.com>
2023-08-10 22:59:50 +0900

    feat: authHandler MSW 설정
    Co-authored-by: Jungkyun Woo <201702042@o.cnu.ac.kr>
    Co-authored-by: Geonyeop Kim <gunyeop530@gmail.com>

---
+++ frontend/src/mocks/handlers/authHandler.ts
@@ -0,0 +1,48 @@
+import { rest } from 'msw';
+
+const accessToken =
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxIiwiaWF0IjoxNjkxNTY4NDI4LCJleHAiOjE2OTE1NzIwMjh9.BfGH7jBxO_iixmlpzxHKV7d9ekJPegLxrpY9ME066ro';
+
+export const authHandler = [
+  rest.post('/api/auth/guest', (req, res, ctx) => {
+    return res(
+      ctx.delay(2000),
+      ctx.json({
+        accessToken,
+      }),
+    );
+  }),
+
+  rest.post('/api/auth/login', (req, res, ctx) => {
+    return res(
+      ctx.delay(2000),
+      ctx.json({
+        accessToken,
+      }),
+      ctx.cookie('refreshToken', Date.now().toString()),
+    );
+  }),
+
+  rest.get('/api/members/:memberId', (req, res, ctx) => {
+    return res(
+      ctx.delay(1000),
+      ctx.json({
+        id: '1',
+        name: '맘모스',
+        email: 'haru12@gmail.com',
+        imageUrl: 'https://lh3.google.com/u/0/ogw/AGvuzYZKngVdZecWrpGTnHj7hQRtO5p9tjelI5lvCcsk=s64-c-mo',
+        loginType: 'google',
+      }),
+    );
+  }),
+
+  rest.post('/api/auth/refresh', (req, res, ctx) => {
+    return res(
+      ctx.delay(300),
+      ctx.json({
+        accessToken,
+      }),
+      ctx.cookie('refreshToken', Date.now().toString()),
+    );
+  }),
+];
