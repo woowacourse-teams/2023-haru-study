ecf3d9ad846555d3db3d83a02c490e7606dd24ac
nlom0218 <nlom0218@naver.com>
2023-08-10 22:57:31 +0900

    chore: webpack Dotenv 설정
    Co-authored-by: Jungkyun Woo <201702042@o.cnu.ac.kr>
    Co-authored-by: Geonyeop Kim <gunyeop530@gmail.com>

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -62,7 +62,7 @@ module.exports = () => {
       }),
 
       new Dotenv({
-        path: './.env.development',
+        path: isDevelopment ? './.env.development' : './.env.production',
       }),
     ],
   };
