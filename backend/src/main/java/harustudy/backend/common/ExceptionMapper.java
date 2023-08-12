551c6a0399093ddc0ffdb3df4fee4744ade3e3ed
woosung1223 <dntjd991223@naver.com>
2023-08-12 15:18:04 +0900

    refactor: 도메인 별 에러코드 정의

--- backend/src/main/java/harustudy/backend/common/ExceptionMapper.java
+++ backend/src/main/java/harustudy/backend/common/ExceptionMapper.java
@@ -35,53 +35,53 @@ public class ExceptionMapper {
     }
 
     private static void setupMemberException() {
-        mapper.put(MemberNotFoundException.class,
-                ExceptionSituation.of("해당하는 멤버가 없습니다.", NOT_FOUND));
-        mapper.put(MemberNameLengthException.class,
-                ExceptionSituation.of("멤버의 닉네임 길이가 유효하지 않습니다.", BAD_REQUEST));
         mapper.put(MemberNotParticipatedException.class,
                 ExceptionSituation.of("멤버가 해당 스터디에 참여하지 않았습니다.", NOT_FOUND, 1000));
+        mapper.put(MemberNameLengthException.class,
+                ExceptionSituation.of("멤버의 닉네임 길이가 유효하지 않습니다.", BAD_REQUEST, 1001));
+        mapper.put(MemberNotFoundException.class,
+                ExceptionSituation.of("해당하는 멤버가 없습니다.", NOT_FOUND, 1002));
     }
 
     private static void setupPomodoroContentException() {
         mapper.put(PomodoroContentNotFoundException.class,
-                ExceptionSituation.of("해당하는 컨텐츠가 없습니다.", NOT_FOUND));
+                ExceptionSituation.of("해당하는 컨텐츠가 없습니다.", NOT_FOUND, 1100));
     }
 
     private static void setupParticipantCodeException() {
         mapper.put(ParticipantCodeNotFoundException.class,
-                ExceptionSituation.of("해당하는 참여코드가 없습니다.", NOT_FOUND));
+                ExceptionSituation.of("해당하는 참여코드가 없습니다.", NOT_FOUND, 1200));
     }
 
     private static void setupPomodoroProgressException() {
         mapper.put(PomodoroProgressNotFoundException.class,
-                ExceptionSituation.of("해당 스터디에 참여한 상태가 아닙니다.", NOT_FOUND));
+                ExceptionSituation.of("해당 스터디에 참여한 상태가 아닙니다.", NOT_FOUND, 1201));
         mapper.put(PomodoroProgressStatusException.class,
-                ExceptionSituation.of("스터디 진행 상태가 적절하지 않습니다.", BAD_REQUEST));
+                ExceptionSituation.of("스터디 진행 상태가 적절하지 않습니다.", BAD_REQUEST, 1202));
         mapper.put(ProgressNotBelongToRoomException.class,
-                ExceptionSituation.of("해당 스터디에 참여한 기록이 없습니다.", BAD_REQUEST));
+                ExceptionSituation.of("해당 스터디에 참여한 기록이 없습니다.", BAD_REQUEST, 1203));
     }
 
     private static void setupRoomException() {
         mapper.put(RoomNotFoundException.class,
-                ExceptionSituation.of("해당하는 스터디가 없습니다.", NOT_FOUND));
+                ExceptionSituation.of("해당하는 스터디가 없습니다.", NOT_FOUND, 1300));
         mapper.put(DuplicatedNicknameException.class,
-                ExceptionSituation.of("멤버 이름은 중복될 수 없습니다.", BAD_REQUEST));
+                ExceptionSituation.of("멤버 이름은 중복될 수 없습니다.", BAD_REQUEST, 1301));
         mapper.put(PomodoroRoomNameLengthException.class,
-                ExceptionSituation.of("스터디 이름의 길이가 적절하지 않습니다.", BAD_REQUEST));
+                ExceptionSituation.of("스터디 이름의 길이가 적절하지 않습니다.", BAD_REQUEST, 1302));
         mapper.put(PomodoroTimePerCycleException.class,
-                ExceptionSituation.of("시간 당 사이클 횟수가 적절하지 않습니다.", BAD_REQUEST));
+                ExceptionSituation.of("시간 당 사이클 횟수가 적절하지 않습니다.", BAD_REQUEST, 1303));
         mapper.put(PomodoroTotalCycleException.class,
-                ExceptionSituation.of("총 사이클 횟수가 적절하지 않습니다.", BAD_REQUEST));
+                ExceptionSituation.of("총 사이클 횟수가 적절하지 않습니다.", BAD_REQUEST, 1304));
     }
 
     private static void setupAuthException() {
         mapper.put(InvalidProviderNameException.class,
-                ExceptionSituation.of("유효하지 않은 프로바이더 이름입니다.", BAD_REQUEST));
+                ExceptionSituation.of("유효하지 않은 프로바이더 이름입니다.", BAD_REQUEST, 1400));
         mapper.put(InvalidRefreshTokenException.class,
-                ExceptionSituation.of("유효하지 않은 갱신 토큰입니다.", BAD_REQUEST));
+                ExceptionSituation.of("유효하지 않은 갱신 토큰입니다.", BAD_REQUEST, 1401));
         mapper.put(RefreshTokenExpiredException.class,
-                ExceptionSituation.of("만료된 갱신 토큰입니다.", UNAUTHORIZED));
+                ExceptionSituation.of("만료된 갱신 토큰입니다.", UNAUTHORIZED, 1402));
     }
 
     public static ExceptionSituation getSituationOf(Exception exception) {
