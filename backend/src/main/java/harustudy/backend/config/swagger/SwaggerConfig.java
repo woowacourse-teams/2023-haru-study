481b1b4d6f4761bd7d1b73518639fdecf0a71a55
jaehee329 <jjhs9803@gmail.com>
2023-08-12 12:11:59 +0900

    refactor: 메서드 분리 및 순서 재정렬

--- backend/src/main/java/harustudy/backend/config/swagger/SwaggerConfig.java
+++ backend/src/main/java/harustudy/backend/config/swagger/SwaggerConfig.java
@@ -41,26 +41,37 @@ public class SwaggerConfig {
     private void setUpApiResponses(Class<? extends HaruStudyException>[] exceptionClasses,
             ApiResponses responses) {
         Arrays.stream(exceptionClasses)
-                .forEach(exceptionClass -> {
-                    try {
-                        HaruStudyException exception = extractExceptionFrom(exceptionClass);
-                        ApiResponse apiResponse = setupApiResponse(exception);
-                        responses.addApiResponse(removePostfix(exceptionClass), apiResponse);
-                    } catch (Exception e) {
-                        throw new RuntimeException(e);
-                    }
-                });
+                .forEach(exceptionClass -> setUpApiResponses(exceptionClass, responses));
     }
 
-    private String removePostfix(Class<? extends HaruStudyException> exceptionClass) {
-        String exceptionClassName = exceptionClass.getSimpleName();
-        return exceptionClassName.substring(0, exceptionClassName.indexOf("Exception"));
+    private void setUpApiResponses(Class<? extends HaruStudyException> exceptionClass,
+            ApiResponses responses) {
+        HaruStudyException exception = extractExceptionFrom(exceptionClass);
+        ApiResponse apiResponse = setupApiResponse(exception);
+        responses.addApiResponse(removePostfix(exceptionClass), apiResponse);
     }
+
 
     private HaruStudyException extractExceptionFrom(
-            Class<? extends HaruStudyException> exceptionClass) throws Exception {
-        Constructor<? extends HaruStudyException> constructor = exceptionClass.getConstructor();
-        return constructor.newInstance();
+            Class<? extends HaruStudyException> exceptionClass) {
+        try {
+            Constructor<? extends HaruStudyException> constructor = exceptionClass.getConstructor();
+            return constructor.newInstance();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private ApiResponse setupApiResponse(HaruStudyException exception) {
+        ObjectSchema objectSchema = setupObjectSchema(exception);
+        MediaType mediaType = new MediaType().schema(objectSchema);
+        Content content = new Content().addMediaType("application/json", mediaType);
+
+        ExceptionSituation situation = ExceptionMapper.getSituationOf(exception);
+        ApiResponse apiResponse = new ApiResponse();
+        apiResponse.setContent(content);
+        apiResponse.description(situation.getStatusCode().toString());
+        return apiResponse;
     }
 
     private ObjectSchema setupObjectSchema(HaruStudyException exception) {
@@ -68,21 +79,13 @@ public class SwaggerConfig {
         ObjectSchema responseObject = new ObjectSchema();
         responseObject.addProperty("message", new StringSchema().example(situation.getMessage()));
         Optional.ofNullable(situation.getErrorCode())
-                .ifPresent(code -> responseObject.addProperty("code", new StringSchema().example(code)));
+                .ifPresent(code -> responseObject.addProperty("code",
+                        new StringSchema().example(code)));
         return responseObject;
     }
 
-    private ApiResponse setupApiResponse(HaruStudyException exception) {
-        ObjectSchema objectSchema = setupObjectSchema(exception);
-        Content content = new Content();
-        MediaType mediaType = new MediaType();
-        ApiResponse apiResponse = new ApiResponse();
-
-        content.addMediaType("application/json", mediaType);
-        ExceptionSituation situation = ExceptionMapper.getSituationOf(exception);
-        mediaType.schema(objectSchema);
-        apiResponse.setContent(content);
-        apiResponse.description(situation.getStatusCode().toString());
-        return apiResponse;
+    private String removePostfix(Class<? extends HaruStudyException> exceptionClass) {
+        String exceptionClassName = exceptionClass.getSimpleName();
+        return exceptionClassName.substring(0, exceptionClassName.indexOf("Exception"));
     }
 }
