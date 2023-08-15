1af12b58002f7f07e77a2ca5e2020714b64352ef
woosung1223 <dntjd991223@naver.com>
2023-08-15 16:13:06 +0900

    docs: `Authenticated` swagger 명세에서 배제

--- backend/src/main/java/harustudy/backend/auth/Authenticated.java
+++ backend/src/main/java/harustudy/backend/auth/Authenticated.java
@@ -1,10 +1,12 @@
 package harustudy.backend.auth;
 
+import io.swagger.v3.oas.annotations.Hidden;
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+@Hidden
 @Target(ElementType.PARAMETER)
 @Retention(RetentionPolicy.RUNTIME)
 public @interface Authenticated {
