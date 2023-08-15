3318efdf338a0b435a5d72e40038d9037d453a7a
yeopto <gunyeop530@gmail.com>
2023-08-15 14:15:21 +0900

    refactor: useCreateStudy 인증 로직 추가

--- frontend/src/hooks/create/useCreateStudy.ts
+++ frontend/src/hooks/create/useCreateStudy.ts
@@ -1,11 +1,39 @@
 import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
-import { requestCreateStudy } from '@Apis/index';
+import { ROUTES_PATH } from '@Constants/routes';
+
+import { boolCheckCookie } from '@Utils/cookie';
+
+import { requestAccessTokenRefresh, requestCreateStudy } from '@Apis/index';
 
 import type { StudyTimePerCycleOptions, TotalCycleOptions } from '@Types/study';
 
+import { ExpiredAccessTokenError } from '../../errors/CustomError';
+
 const useCreateStudy = (errorHandler: (error: Error) => void) => {
   const [isLoading, setIsLoading] = useState(false);
+
+  const navigate = useNavigate();
+
+  const getAccessTokenRefresh = async () => {
+    try {
+      const hasRefreshToken = boolCheckCookie('refreshToken');
+
+      if (!hasRefreshToken) {
+        navigate(ROUTES_PATH.login);
+        return;
+      }
+
+      const { accessToken } = await requestAccessTokenRefresh();
+
+      sessionStorage.setItem('accessToken', accessToken);
+
+      return accessToken;
+    } catch (error) {
+      if (error instanceof Error) return errorHandler(error);
+    }
+  };
 
   const createStudy = async (
     studyName: string,
@@ -15,10 +43,25 @@ const useCreateStudy = (errorHandler: (error: Error) => void) => {
     setIsLoading(true);
 
     try {
-      return await requestCreateStudy(studyName, totalCycle, timePerCycle);
+      const accessToken = sessionStorage.getItem('accessToken');
+
+      if (!accessToken) {
+        const error = new Error('토큰이 없습니다. 다시 로그인 해주세요.');
+        errorHandler(error);
+        navigate(`${ROUTES_PATH.login}`);
+        return;
+      }
+
+      return await requestCreateStudy(studyName, totalCycle, timePerCycle, accessToken);
     } catch (error) {
-      if (!(error instanceof Error)) throw error;
-      errorHandler(error);
+      if (error instanceof ExpiredAccessTokenError) {
+        const accessToken = await getAccessTokenRefresh();
+
+        if (accessToken) return await requestCreateStudy(studyName, totalCycle, timePerCycle, accessToken);
+
+        return;
+      }
+      if (error instanceof Error) return errorHandler(error);
     } finally {
       setIsLoading(false);
     }
