d3cb26d78b51cd71bb96dc14aa49dc1cc98961c6
MoonJeWoong <munjin0201@naver.com>
2023-08-15 22:45:28 +0900

    test: memberService 인가 테스트 추가

--- backend/src/test/java/harustudy/backend/member/service/MemberServiceTest.java
+++ backend/src/test/java/harustudy/backend/member/service/MemberServiceTest.java
@@ -1,123 +1,78 @@
-//package harustudy.backend.member.service;
-//
-//import static org.assertj.core.api.Assertions.assertThat;
-//import static org.assertj.core.api.SoftAssertions.assertSoftly;
-//
-//import harustudy.backend.content.domain.PomodoroContent;
-//import harustudy.backend.member.domain.Member;
-//import harustudy.backend.member.dto.GuestRegisterRequest;
-//import harustudy.backend.member.dto.MemberResponseV2;
-//import harustudy.backend.member.dto.MembersResponseV2;
-//import harustudy.backend.room.domain.CodeGenerationStrategy;
-//import harustudy.backend.room.domain.ParticipantCode;
-//import harustudy.backend.progress.domain.PomodoroProgress;
-//import harustudy.backend.room.domain.PomodoroRoom;
-//import jakarta.persistence.EntityManager;
-//import jakarta.persistence.PersistenceContext;
-//import java.util.List;
-//import org.assertj.core.api.Condition;
-//import org.junit.jupiter.api.DisplayNameGeneration;
-//import org.junit.jupiter.api.DisplayNameGenerator.ReplaceUnderscores;
-//import org.junit.jupiter.api.Test;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.boot.test.context.SpringBootTest;
-//import org.springframework.transaction.annotation.Transactional;
-//
-//@SuppressWarnings("NonAsciiCharacters")
-//@DisplayNameGeneration(ReplaceUnderscores.class)
-//@SpringBootTest
-//@Transactional
-//class MemberServiceV2Test {
-//
-//    @PersistenceContext
-//    EntityManager entityManager;
-//    @Autowired
-//    MemberServiceV2 memberServiceV2;
-//
-//    @Test
-//    void 멤버_아이디로_멤버_엔티티를_조회한다() {
-//        // given
-//        Member member = new Member("member1");
-//        entityManager.persist(member);
-//        entityManager.flush();
-//        entityManager.clear();
-//
-//        // when
-//        MemberResponseV2 response = memberServiceV2.findMember(member.getId());
-//
-//        // then
-//        assertSoftly(softly -> {
-//            assertThat(response.id()).isEqualTo(member.getId());
-//            assertThat(response.nickname()).isEqualTo(member.getNickname());
-//        });
-//    }
-//
-//    @Test
-//    void 방_아이디로_해당_방에_참여한_멤버들을_모두_조회한다() {
-//        // given
-//        ParticipantCode participantCode = new ParticipantCode(new CodeGenerationStrategy());
-//        PomodoroRoom room =
-//                new PomodoroRoom("roomName", 1, 20, participantCode);
-//
-//        Member member1 = new Member("member1");
-//        Member member2 = new Member("member2");
-//        PomodoroProgress pomodoroProgress1 = new PomodoroProgress(room, member1);
-//        PomodoroProgress pomodoroProgress2 = new PomodoroProgress(room, member2);
-//
-//        entityManager.persist(participantCode);
-//        entityManager.persist(room);
-//        entityManager.persist(member1);
-//        entityManager.persist(member2);
-//        entityManager.persist(pomodoroProgress1);
-//        entityManager.persist(pomodoroProgress2);
-//
-//        entityManager.flush();
-//        entityManager.clear();
-//
-//        // when
-//        MembersResponseV2 response = memberServiceV2.findParticipatedMembers(room.getId());
-//
-//        // then
-//        Condition<String> condition = new Condition<>(
-//                s -> s.equals(member1.getNickname()) || s.equals(member2.getNickname()),
-//                "member1의 nickname과 같거나 member2의 nickname과 같다"
-//        );
-//
-//        assertSoftly(softly -> {
-//            assertThat(response.members().size()).isEqualTo(2);
-//            assertThat(response.members().get(0).nickname()).is(condition);
-//            assertThat(response.members().get(1).nickname()).is(condition);
-//        });
-//    }
-//
-//    @Test
-//    void 스터디_아이디와_닉네임으로_스터디에_멤버를_등록한다() {
-//        // given
-//        ParticipantCode participantCode = new ParticipantCode(new CodeGenerationStrategy());
-//        PomodoroRoom room =
-//                new PomodoroRoom("roomName", 3, 20, participantCode);
-//        entityManager.persist(participantCode);
-//        entityManager.persist(room);
-//        GuestRegisterRequest request = new GuestRegisterRequest(room.getId(), "nickname");
-//
-//        // when
-//        Long memberId = memberServiceV2.register(request);
-//
-//        // then
-//        entityManager.flush();
-//        entityManager.clear();
-//
-//        Member registeredMember = entityManager.find(Member.class, memberId);
-//        PomodoroRoom registeredPomodoroRoom = entityManager.find(PomodoroRoom.class, room.getId());
-//        List<PomodoroProgress> pomodoroProgresses = registeredPomodoroRoom.getPomodoroProgresses();
-//        PomodoroProgress registeredPomodoroProgress = pomodoroProgresses.get(0);
-//        List<PomodoroContent> pomodoroContents = registeredPomodoroProgress.getPomodoroContents();
-//
-//        assertSoftly(softly -> {
-//            assertThat(registeredMember.getNickname()).isEqualTo(request.nickname());
-//            assertThat(registeredPomodoroRoom.getName()).isEqualTo(room.getName());
-//            assertThat(pomodoroProgresses.size()).isEqualTo(1);
-//            assertThat(pomodoroContents.size()).isEqualTo(3);
-//        });
-//    }
-//}
+package harustudy.backend.member.service;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.assertj.core.api.SoftAssertions.assertSoftly;
+
+import harustudy.backend.auth.dto.AuthMember;
+import harustudy.backend.member.domain.LoginType;
+import harustudy.backend.member.domain.Member;
+import harustudy.backend.member.dto.MemberResponse;
+import harustudy.backend.member.exception.MemberHasInvalidAuthorizationException;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayNameGeneration;
+import org.junit.jupiter.api.DisplayNameGenerator.ReplaceUnderscores;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+@SuppressWarnings("NonAsciiCharacters")
+@DisplayNameGeneration(ReplaceUnderscores.class)
+@SpringBootTest
+@Transactional
+class MemberServiceTest {
+
+    @PersistenceContext
+    private EntityManager entityManager;
+    @Autowired
+    private MemberService memberService;
+
+    private Member member1;
+    private Member member2;
+
+    @BeforeEach
+    void setUp() {
+        member1 = new Member("memberName1", "member1@gmail.com", "member1ProfileImageUrl",
+                LoginType.GOOGLE);
+        member2 = new Member("memberName2", "member2@gmail.com", "member2ProfileImageUrl",
+                LoginType.GOOGLE);
+
+        entityManager.persist(member1);
+        entityManager.persist(member2);
+        entityManager.flush();
+        entityManager.clear();
+    }
+
+    @Test
+    void 인증된_멤버가_자신의_멤버정보를_조회한다() {
+        // given
+        AuthMember authMember = new AuthMember(member1.getId());
+
+        // when
+        MemberResponse foundMember = memberService.findMember(authMember, member1.getId());
+
+        //then
+        assertSoftly(softly -> {
+            assertThat(foundMember.memberId()).isEqualTo(member1.getId());
+            assertThat(foundMember.name()).isEqualTo(member1.getName());
+            assertThat(foundMember.email()).isEqualTo(member1.getEmail());
+            assertThat(foundMember.imageUrl()).isEqualTo(member1.getImageUrl());
+            assertThat(foundMember.loginType()).isEqualTo(
+                    member1.getLoginType().name().toLowerCase());
+        });
+    }
+
+    @Test
+    void 인증된_멤버가_다른_멤버정보를_조회하면_인가_예외처리한다() {
+        // given
+        AuthMember authMember = new AuthMember(member1.getId());
+
+        // when, then
+        assertThatThrownBy(
+                () -> memberService.findMember(authMember, member2.getId())).isInstanceOf(
+                MemberHasInvalidAuthorizationException.class);
+    }
+}
