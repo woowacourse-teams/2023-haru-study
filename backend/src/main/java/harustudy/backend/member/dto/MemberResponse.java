453eb324a4e33c15fa2193d1d534e2d82c3adbd6
MoonJeWoong <munjin0201@naver.com>
2023-08-15 19:00:28 +0900

    feat: member 인가 기능 구현

--- backend/src/main/java/harustudy/backend/member/dto/MemberResponse.java
+++ backend/src/main/java/harustudy/backend/member/dto/MemberResponse.java
@@ -1,6 +1,12 @@
 package harustudy.backend.member.dto;
 
+import harustudy.backend.member.domain.Member;
+
 public record MemberResponse(Long memberId, String name, String email, String imageUrl,
                              String loginType) {
 
+    public static MemberResponse from(Member member) {
+        return new MemberResponse(member.getId(), member.getName(), member.getEmail(),
+                member.getImageUrl(), member.getLoginType().name().toLowerCase());
+    }
 }
--- backend/src/main/java/harustudy/backend/member/service/MemberService.java
+++ backend/src/main/java/harustudy/backend/member/service/MemberService.java
@@ -1,62 +1,31 @@
-//package harustudy.backend.member.service;
-//
-//import harustudy.backend.content.domain.PomodoroContent;
-//import harustudy.backend.content.repository.PomodoroContentRepository;
-//import harustudy.backend.member.domain.Member;
-//import harustudy.backend.member.dto.GuestRegisterRequest;
-//import harustudy.backend.member.dto.MemberResponseV2;
-//import harustudy.backend.member.dto.MembersResponseV2;
-//import harustudy.backend.member.exception.MemberNotFoundException;
-//import harustudy.backend.member.repository.MemberRepository;
-//import harustudy.backend.progress.domain.PomodoroProgress;
-//import harustudy.backend.progress.repository.PomodoroProgressRepository;
-//import harustudy.backend.room.domain.PomodoroRoom;
-//import harustudy.backend.room.exception.RoomNotFoundException;
-//import harustudy.backend.room.repository.PomodoroRoomRepository;
-//import java.util.List;
-//import lombok.RequiredArgsConstructor;
-//import org.springframework.stereotype.Service;
-//import org.springframework.transaction.annotation.Transactional;
-//
-//@RequiredArgsConstructor
-//@Transactional
-//@Service
-//public class MemberServiceV2 {
-//
-//    private final MemberRepository memberRepository;
-//    private final PomodoroRoomRepository pomodoroRoomRepository;
-//    private final PomodoroProgressRepository pomodoroProgressRepository;
-//    private final PomodoroContentRepository pomodoroContentRepository;
-//
-//    public MemberResponseV2 findMember(Long memberId) {
-//        Member member = memberRepository.findById(memberId)
-//                .orElseThrow(MemberNotFoundException::new);
-//        return MemberResponseV2.from(member);
-//    }
-//
-//    public MembersResponseV2 findParticipatedMembers(Long roomId) {
-//        PomodoroRoom pomodoroRoom = pomodoroRoomRepository.findById(roomId)
-//                .orElseThrow(RoomNotFoundException::new);
-//        List<PomodoroProgress> pomodoroProgresses =
-//                pomodoroProgressRepository.findAllByPomodoroRoomFetchMember(pomodoroRoom);
-//        List<MemberResponseV2> memberResponses = pomodoroProgresses.stream()
-//                .map(PomodoroProgress::getMember)
-//                .map(MemberResponseV2::from)
-//                .toList();
-//        return new MembersResponseV2(memberResponses);
-//    }
-//
-//    public Long register(GuestRegisterRequest request) {
-//        Member member = memberRepository.save(new Member(request.nickname()));
-//        PomodoroRoom pomodoroRoom = pomodoroRoomRepository.findById(request.studyId())
-//                .orElseThrow(RoomNotFoundException::new);
-//        Integer totalCycle = pomodoroRoom.getTotalCycle();
-//
-//        PomodoroProgress pomodoroProgress = pomodoroProgressRepository.save(
-//                new PomodoroProgress(pomodoroRoom, member));
-//        for (int i = 1; i <= totalCycle; i++) {
-//            pomodoroContentRepository.save(new PomodoroContent(pomodoroProgress, i));
-//        }
-//        return member.getId();
-//    }
-//}
+package harustudy.backend.member.service;
+
+import harustudy.backend.auth.dto.AuthMember;
+import harustudy.backend.member.domain.Member;
+import harustudy.backend.member.dto.MemberResponse;
+import harustudy.backend.member.exception.MemberHasInvalidAuthorizationException;
+import harustudy.backend.member.exception.MemberNotFoundException;
+import harustudy.backend.member.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@RequiredArgsConstructor
+@Transactional
+@Service
+public class MemberService {
+
+    private final MemberRepository memberRepository;
+
+    public MemberResponse findMember(AuthMember authMember, Long memberId) {
+        Member authorizedMember = memberRepository.findById(authMember.id())
+                .orElseThrow(MemberNotFoundException::new);
+        Member foundMember = memberRepository.findById(memberId)
+                .orElseThrow(MemberNotFoundException::new);
+
+        if (authorizedMember.isDifferentMember(foundMember)) {
+            throw new MemberHasInvalidAuthorizationException();
+        }
+        return MemberResponse.from(foundMember);
+    }
+}
