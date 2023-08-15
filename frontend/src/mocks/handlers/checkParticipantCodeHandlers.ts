286e92da813ddab5327e5338c925a8798c533db8
yeopto <gunyeop530@gmail.com>
2023-08-15 17:53:26 +0900

    test: accessToken에 관련한 msw 로직 추가 및 api 변경안 적용

--- frontend/src/mocks/handlers/checkParticipantCodeHandlers.ts
+++ frontend/src/mocks/handlers/checkParticipantCodeHandlers.ts
@@ -1,25 +1,51 @@
 import { rest } from 'msw';
 
-type RequestData = {
-  participantCode: string;
-  memberId: number | null;
-};
-
 export const checkParticipantCodeHandlers = [
-  rest.post('api/studies/authenticate', async (req, res, ctx) => {
+  rest.get('api/v2/studies', async (req, res, ctx) => {
+    const accessToken =
+      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxIiwiaWF0IjoxNjkxNTY4NDI4LCJleHAiOjE2OTE1NzIwMjh9.BfGH7jBxO_iixmlpzxHKV7d9ekJPegLxrpY9ME066ro';
+    const requestAuthToken = req.headers.get('Authorization')?.split(' ')[1];
+    const newAccessToken =
+      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxIiwiaWF0IjoxMjM0NTY3fQ.NUiutjXo0mcIBU5fWxfjpBEvPxakFiBaUCg4THKAYpQ';
+
     const testParticipantCode = '123456';
+    const participantCode = req.url.searchParams.get('participantCode');
 
-    const { participantCode }: RequestData = await req.json();
+    if (requestAuthToken === newAccessToken && testParticipantCode === participantCode)
+      return res(
+        ctx.status(200),
+        ctx.json({
+          studies: [
+            {
+              studyId: 1,
+              name: '안오면 지상렬',
+              totalCycle: 2,
+              timePerCycle: 20,
+              createdDateTime: Date.now.toString(),
+            },
+          ],
+        }),
+        ctx.delay(1000),
+      );
 
-    if (testParticipantCode !== participantCode) {
-      return res(ctx.status(404));
-    }
+    if (requestAuthToken === newAccessToken && testParticipantCode !== participantCode) return res(ctx.status(404));
+
+    if (accessToken !== requestAuthToken) return res(ctx.status(401), ctx.delay(1000));
+
+    if (accessToken === requestAuthToken && testParticipantCode !== participantCode) return res(ctx.status(404));
 
     return res(
       ctx.status(200),
       ctx.json({
-        studyId: 1,
-        studyName: '안오면 지상렬',
+        studies: [
+          {
+            studyId: 1,
+            name: '안오면 지상렬',
+            totalCycle: 2,
+            timePerCycle: 20,
+            createdDateTime: Date.now.toString(),
+          },
+        ],
       }),
       ctx.delay(1000),
     );
