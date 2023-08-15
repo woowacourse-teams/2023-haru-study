f422847bcb00ccfe44f39c92fdec7cbac686d20a
yeopto <gunyeop530@gmail.com>
2023-08-15 17:49:42 +0900

    refactor: 인증 로직 추가 및 api response 값 변경에 따른 로직 수정

--- frontend/src/hooks/participation/useParticipationCode.ts
+++ frontend/src/hooks/participation/useParticipationCode.ts
@@ -1,18 +1,68 @@
 import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
-import { requestAuthenticateParticipationCode } from '@Apis/index';
+import { ROUTES_PATH } from '@Constants/routes';
+
+import { boolCheckCookie } from '@Utils/cookie';
+
+import { requestAccessTokenRefresh, requestAuthenticateParticipationCode } from '@Apis/index';
+
+import { ExpiredAccessTokenError } from '../../errors/CustomError';
 
 const useParticipationCode = (errorHandler: (error: Error) => void) => {
   const [isLoading, setIsLoading] = useState(false);
+
+  const navigate = useNavigate();
+
+  const getAccessTokenRefresh = async () => {
+    try {
+      const hasRefreshToken = boolCheckCookie('refreshToken');
+
+      if (!hasRefreshToken) {
+        navigate(ROUTES_PATH.login);
+        return;
+      }
+
+      const { accessToken } = await requestAccessTokenRefresh();
+
+      sessionStorage.setItem('accessToken', accessToken);
+
+      return accessToken;
+    } catch (error) {
+      if (error instanceof Error) return errorHandler(error);
+    }
+  };
+
+  const newRequestAuthenticateParticipationCode = async (participantCode: string, accessToken: string) => {
+    try {
+      return await requestAuthenticateParticipationCode(participantCode, accessToken);
+    } catch (error) {
+      if (error instanceof Error) return errorHandler(error);
+    }
+  };
 
   const authenticateParticipationCode = async (participantCode: string) => {
     setIsLoading(true);
 
     try {
-      return await requestAuthenticateParticipationCode(participantCode);
+      const accessToken = sessionStorage.getItem('accessToken');
+
+      if (!accessToken) {
+        const error = new Error('토큰이 없습니다. 다시 로그인 해주세요.');
+        errorHandler(error);
+        navigate(`${ROUTES_PATH.login}`);
+        return;
+      }
+
+      return await requestAuthenticateParticipationCode(participantCode, accessToken);
     } catch (error) {
-      if (!(error instanceof Error)) throw error;
-      errorHandler(error);
+      if (error instanceof ExpiredAccessTokenError) {
+        const accessToken = await getAccessTokenRefresh();
+
+        if (accessToken) return await newRequestAuthenticateParticipationCode(participantCode, accessToken);
+      }
+
+      if (error instanceof Error) return errorHandler(error);
     } finally {
       setIsLoading(false);
     }
