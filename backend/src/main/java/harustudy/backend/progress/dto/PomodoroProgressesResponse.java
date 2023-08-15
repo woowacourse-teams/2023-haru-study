ade015e55d31d3c7563eba53c6fccbad2ce553b4
woosung1223 <dntjd991223@naver.com>
2023-08-15 20:19:08 +0900

    chore: DTO 필드명 변경 `pomodoroProgresses` -> `progresses`

--- backend/src/main/java/harustudy/backend/progress/dto/PomodoroProgressesResponse.java
+++ backend/src/main/java/harustudy/backend/progress/dto/PomodoroProgressesResponse.java
@@ -2,7 +2,7 @@ package harustudy.backend.progress.dto;
 
 import java.util.List;
 
-public record PomodoroProgressesResponse(List<PomodoroProgressResponse> pomodoroProgresses) {
+public record PomodoroProgressesResponse(List<PomodoroProgressResponse> progresses) {
 
     public static PomodoroProgressesResponse from(List<PomodoroProgressResponse> responses) {
         return new PomodoroProgressesResponse(responses);
