a1b0f40ac9979582d0f94dbcd71f5d49c765c346
yeopto <gunyeop530@gmail.com>
2023-08-15 23:21:27 +0900

    feat: progresses가 없을 때 로직을 수행하기 위해 CustomError 작성

--- frontend/src/errors/CustomError.ts
+++ frontend/src/errors/CustomError.ts
@@ -7,3 +7,13 @@ export class ExpiredAccessTokenError extends Error {
     this.statusCode = statusCode;
   }
 }
+
+export class NotExistProgressesError extends Error {
+  statusCode: number;
+
+  constructor(message: string, statusCode: number) {
+    super(message);
+    this.name = 'NotExistProgressesError';
+    this.statusCode = statusCode;
+  }
+}
