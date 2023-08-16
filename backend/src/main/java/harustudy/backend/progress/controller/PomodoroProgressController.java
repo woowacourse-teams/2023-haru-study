bfabf3a39ff92504e1c995a7ca69b397f9c768ef
teo <78679830+woosung1223@users.noreply.github.com>
2023-08-16 14:25:48 +0900

    [BE] Progress 관련 Exception 문서화

--- backend/src/main/java/harustudy/backend/progress/controller/PomodoroProgressController.java
+++ backend/src/main/java/harustudy/backend/progress/controller/PomodoroProgressController.java
@@ -2,10 +2,17 @@ package harustudy.backend.progress.controller;
 
 import harustudy.backend.auth.Authenticated;
 import harustudy.backend.auth.dto.AuthMember;
+import harustudy.backend.auth.exception.AuthorizationException;
+import harustudy.backend.common.SwaggerExceptionResponse;
+import harustudy.backend.member.exception.MemberNotFoundException;
 import harustudy.backend.progress.dto.ParticipateStudyRequest;
 import harustudy.backend.progress.dto.PomodoroProgressResponse;
 import harustudy.backend.progress.dto.PomodoroProgressesResponse;
+import harustudy.backend.progress.exception.NicknameLengthException;
+import harustudy.backend.progress.exception.PomodoroProgressNotFoundException;
+import harustudy.backend.progress.exception.ProgressNotBelongToRoomException;
 import harustudy.backend.progress.service.PomodoroProgressService;
+import harustudy.backend.room.exception.RoomNotFoundException;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
@@ -21,6 +28,13 @@ public class PomodoroProgressController {
 
     private final PomodoroProgressService pomodoroProgressService;
 
+    @SwaggerExceptionResponse({
+            MemberNotFoundException.class,
+            RoomNotFoundException.class,
+            PomodoroProgressNotFoundException.class,
+            AuthorizationException.class,
+            ProgressNotBelongToRoomException.class
+    })
     @Operation(summary = "단일 스터디 진행도 조회")
     @GetMapping("/api/studies/{studyId}/progresses/{progressId}")
     public ResponseEntity<PomodoroProgressResponse> findPomodoroProgress(
@@ -33,6 +47,12 @@ public class PomodoroProgressController {
         return ResponseEntity.ok(response);
     }
 
+    @SwaggerExceptionResponse({
+            RoomNotFoundException.class,
+            MemberNotFoundException.class,
+            PomodoroProgressNotFoundException.class,
+            AuthorizationException.class
+    })
     @Operation(summary = "필터링 조건으로 스터디 진행도 조회")
     @GetMapping("/api/studies/{studyId}/progresses")
     public ResponseEntity<PomodoroProgressesResponse> findPomodoroProgressesWithFilter(
@@ -45,6 +65,13 @@ public class PomodoroProgressController {
         return ResponseEntity.ok(response);
     }
 
+    @SwaggerExceptionResponse({
+            MemberNotFoundException.class,
+            PomodoroProgressNotFoundException.class,
+            RoomNotFoundException.class,
+            AuthorizationException.class,
+            ProgressNotBelongToRoomException.class
+    })
     @Operation(summary = "다음 스터디 단계로 이동")
     @ApiResponse(responseCode = "204")
     @PostMapping("/api/studies/{studyId}/progresses/{progressId}/next-step")
@@ -57,6 +84,12 @@ public class PomodoroProgressController {
         return ResponseEntity.noContent().build();
     }
 
+    @SwaggerExceptionResponse({
+            MemberNotFoundException.class,
+            RoomNotFoundException.class,
+            AuthorizationException.class,
+            NicknameLengthException.class
+    })
     @Operation(summary = "스터디 참여")
     @ApiResponse(responseCode = "201")
     @PostMapping("/api/studies/{studyId}/progresses")
