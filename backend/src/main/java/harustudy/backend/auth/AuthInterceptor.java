876ede877454a6272d284bf5a5de4d036c3761b8
teo <78679830+woosung1223@users.noreply.github.com>
2023-08-16 19:07:11 +0900

    [BE] CORS 및 preflight 설정 (#373)
    * feat: CORS 설정 추가
    
    * feat: Preflight 관련 코드 추가

--- backend/src/main/java/harustudy/backend/auth/AuthInterceptor.java
+++ backend/src/main/java/harustudy/backend/auth/AuthInterceptor.java
@@ -6,6 +6,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
 
@@ -18,6 +19,9 @@ public class AuthInterceptor implements HandlerInterceptor {
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response,
             Object handler) throws Exception {
+        if (HttpMethod.OPTIONS.matches(request.getMethod())) {
+            return true;
+        }
         String authorizationHeader = request.getHeader(HttpHeaders.AUTHORIZATION);
         String[] splitAuthorizationHeader = authorizationHeader.split(" ");
         if (splitAuthorizationHeader.length != 2 ||
