98929be56c8d8044806aeb42147bedeaddbcf193
Jungkyun Woo <73513965+woo-jk@users.noreply.github.com>
2023-08-16 14:18:29 +0900

    [FE] API 통신 커스텀 에러 (#358)
    * refactor: Custom Error 구현
    
    * refactor: ResponseAPIError 타입 설정
    
    * refactor: http get post 함수 커스텀 에러 적용
    
    * refactor: errors 폴더 alias 설정
    
    * refactor: http 함수 공통 로직 분리

--- frontend/src/types/api.ts
+++ frontend/src/types/api.ts
@@ -1,6 +1,11 @@
 import type { MemberInfo } from './member';
 import type { Member, MemberRecordContent, PlanList, Step, StudyBasicInfo } from './study';
 
+export type ResponseAPIError = {
+  message: string;
+  code: number;
+};
+
 export type ResponseCreateStudy = { participantCode: string; studyName: string };
 
 export type ResponseStudyInfo = {
