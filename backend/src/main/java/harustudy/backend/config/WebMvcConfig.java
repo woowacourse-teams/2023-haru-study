fbf4de35f0c8ae60e5c11e6bb56089cb6f457005
MODI <77962265+jaehee329@users.noreply.github.com>
2023-08-17 10:55:38 +0900

    [BE] CORS 및 preflight 설정 (#384)
    * feat: CORS 설정 추가
    
    * feat: Preflight 관련 코드 추가
    
    * fix: 프로퍼티에서 값을 불러오지 못하던 문제 해결
    
    * chore: test용 yml에 프로퍼티 추가
    
    * fix: cors 설정 변경
    
    ---------
    
    Co-authored-by: woosung1223 <dntjd991223@naver.com>
    Co-authored-by: teo <78679830+woosung1223@users.noreply.github.com>

--- backend/src/main/java/harustudy/backend/config/WebMvcConfig.java
+++ backend/src/main/java/harustudy/backend/config/WebMvcConfig.java
@@ -36,7 +36,7 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**")
-                .allowedOrigins(corsAllowOrigins)
+                .allowedOriginPatterns(corsAllowOrigins)
                 .allowedMethods("*")
                 .allowCredentials(true);
     }
