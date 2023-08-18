866c386dcce09c6222fe4d64a0834432f27c167a
woo-jk <201702042@o.cnu.ac.kr>
2023-08-18 10:06:57 +0900

    feat: 가이드 모달 템플릿 구현

---
+++ frontend/src/components/board/GuideModal/GuideModal.tsx
@@ -0,0 +1,86 @@
+import { styled } from 'styled-components';
+
+import Button from '@Components/common/Button/Button';
+import Typography from '@Components/common/Typography/Typography';
+
+import color from '@Styles/color';
+
+import { useModal } from '@Contexts/ModalProvider';
+
+type RequiredQuestion = 'toDo' | 'completionCondition' | 'doneAsExpected';
+
+const contents = {
+  title: {
+    toDo: '이번 사이클에서 학습할 것은 무엇인가요?',
+    completionCondition: '그 학습의 완료 조건은 무엇인가요?',
+    doneAsExpected: '실제로 학습이 어떻게 됐나요? 예상대로 잘 이루어졌나요?',
+  },
+  descriptions: {
+    toDo: [
+      '• 학습하고자 하는 분야나 주제를 명확하게 정의하세요.',
+      '• 큰 목표를 작은 단계로 나눠보세요.',
+      '• 예시 1) 자바스크립트 딥다이브 챕터15-1 독서 및 정리',
+      '• 예시 2) 영단어 20개 외우기',
+      '• 예시 3) 6월 모의고사 국어 비문학 오답노트 만들기',
+    ],
+    completionCondition: [
+      '• 학습한 내용에 대한 구체적인 결과물을 만들어 보세요.',
+      '• 자기 평가를 통해 얼마나 잘 이해하고 있는지 확인해보세요.',
+      '• 예시 1) 자바스크립트 호이스팅 개념 스터디원에게 설명하기',
+      '• 예시 2) 학습한 영어 단어 테스트하여 만점 받기',
+      '• 예시 3) 오답노트 완성 후 오답노트 1회독하기',
+    ],
+    doneAsExpected: [],
+  },
+} as const;
+
+type Props = {
+  question: RequiredQuestion;
+};
+
+const GuideModal = ({ question }: Props) => {
+  const { closeModal } = useModal();
+
+  return (
+    <GuideLayout>
+      <Typography variant="h6">{contents.title[question]}</Typography>
+      <Separator />
+      <Description>
+        {contents.descriptions[question].map((description) => (
+          <Typography key={description} variant="p2">
+            {description}
+          </Typography>
+        ))}
+      </Description>
+      <Button variant="secondary" size="x-small" $block={false} onClick={closeModal}>
+        확인
+      </Button>
+    </GuideLayout>
+  );
+};
+
+const GuideLayout = styled.div`
+  padding: 5px 10px;
+
+  button {
+    margin-top: 18px;
+    float: right;
+  }
+`;
+
+const Separator = styled.div`
+  width: 100%;
+  height: 1px;
+
+  margin: 24px 0;
+
+  background-color: ${color.neutral[200]};
+`;
+
+const Description = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 12px;
+`;
+
+export default GuideModal;
