33e285405d7298864ae4ddee07b0b75e0751b88d
woosung1223 <dntjd991223@naver.com>
2023-08-22 23:39:27 +0900

    feat: 에러코드용 컨트롤러 생성

---
+++ backend/src/main/java/harustudy/backend/common/ErrorCodeView.java
@@ -0,0 +1,17 @@
+package harustudy.backend.common;
+
+import java.util.List;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class ErrorCodeView {
+
+    @RequestMapping("/error-code")
+    public String errorCodeView(Model model) {
+        List<ExceptionSituation> exceptionSituations = ExceptionMapper.getExceptionSituations();
+        model.addAttribute("exceptionSituations", exceptionSituations);
+        return "error-code";
+    }
+}
