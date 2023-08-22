28d8a24921f392642c61e4e36e1de5a10d686b2b
MODI <77962265+jaehee329@users.noreply.github.com>
2023-08-22 17:34:07 +0900

    [BE] 변경된 테스트 작성 (#437)
    * test: Auth, Member, PomodoroRoom 통합 테스트 추가
    
    * test: 인수 테스트 및 통합 테스트 추가
    
    Co-authored-by: woosung1223 <dntjd991223@naver.com>
    Co-authored-by: MODI <77962265+jaehee329@users.noreply.github.com>
    Co-authored-by: MoonJeWoong <munjin0201@naver.com>
    
    * test: api 변경에 따른 /api/me 테스트 수정
    
    /api/members/{memberId} 통합 테스트를 /api/me 테스트로 변경
    
    ---------
    
    Co-authored-by: aak2075 <lks971003@gmail.com>
    Co-authored-by: woosung1223 <dntjd991223@naver.com>
    Co-authored-by: MoonJeWoong <munjin0201@naver.com>

--- backend/src/test/java/harustudy/backend/progress/repository/PomodoroProgressRepositoryTest.java
+++ backend/src/test/java/harustudy/backend/progress/repository/PomodoroProgressRepositoryTest.java
@@ -1,125 +1,129 @@
-//package harustudy.backend.progress.repository;
-//
-//import static org.assertj.core.api.Assertions.assertThat;
-//import static org.assertj.core.api.SoftAssertions.assertSoftly;
-//
-//import harustudy.backend.member.domain.Member;
-//import harustudy.backend.member.repository.MemberRepository;
-//import harustudy.backend.room.domain.CodeGenerationStrategy;
-//import harustudy.backend.room.domain.ParticipantCode;
-//import harustudy.backend.room.repository.ParticipantCodeRepository;
-//import harustudy.backend.progress.domain.PomodoroProgress;
-//import harustudy.backend.room.domain.PomodoroRoom;
-//import harustudy.backend.room.repository.PomodoroRoomRepository;
-//import jakarta.persistence.PersistenceUtil;
-//import java.util.List;
-//import java.util.Optional;
-//import org.junit.jupiter.api.DisplayNameGeneration;
-//import org.junit.jupiter.api.DisplayNameGenerator.ReplaceUnderscores;
-//import org.junit.jupiter.api.Test;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-//import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
-//
-//@SuppressWarnings("NonAsciiCharacters")
-//@DisplayNameGeneration(ReplaceUnderscores.class)
-//@DataJpaTest
-//class PomodoroProgressRepositoryTest {
-//
-//    @Autowired
-//    private PomodoroProgressRepository pomodoroProgressRepository;
-//    @Autowired
-//    private PomodoroRoomRepository pomodoroRoomRepository;
-//    @Autowired
-//    private MemberRepository memberRepository;
-//    @Autowired
-//    private ParticipantCodeRepository participantCodeRepository;
-//
-//    @Autowired
-//    private TestEntityManager testEntityManager;
-//
-//    @Test
-//    void room과_member를_통해_pomodoroProgress를_조회한다() {
-//        // given
-//        Member member = new Member("member");
-//        ParticipantCode participantCode = new ParticipantCode(new CodeGenerationStrategy());
-//        PomodoroRoom pomodoroRoom = new PomodoroRoom("roomName", 1, 20, participantCode);
-//        memberRepository.save(member);
-//        participantCodeRepository.save(participantCode);
-//        pomodoroRoomRepository.save(pomodoroRoom);
-//
-//        PomodoroProgress pomodoroProgress = new PomodoroProgress(pomodoroRoom, member);
-//        pomodoroProgressRepository.save(pomodoroProgress);
-//
-//        testEntityManager.flush();
-//        testEntityManager.clear();
-//
-//        // when
-//        Optional<PomodoroProgress> found = pomodoroProgressRepository.findByPomodoroRoomAndMember(
-//                pomodoroRoom, member);
-//
-//        // then
-//        assertThat(found).isPresent();
-//        assertThat(found.get().getId()).isEqualTo(pomodoroProgress.getId());
-//    }
-//
-//    @Test
-//    void room으로_pomodoroProgress_리스트를_조회한다() {
-//        // given
-//        Member member1 = new Member("member1");
-//        Member member2 = new Member("member2");
-//        ParticipantCode participantCode = new ParticipantCode(new CodeGenerationStrategy());
-//        PomodoroRoom pomodoroRoom = new PomodoroRoom("roomName", 1, 20, participantCode);
-//        memberRepository.save(member1);
-//        memberRepository.save(member2);
-//        participantCodeRepository.save(participantCode);
-//        pomodoroRoomRepository.save(pomodoroRoom);
-//
-//        PomodoroProgress pomodoroProgress1 = new PomodoroProgress(pomodoroRoom, member1);
-//        PomodoroProgress pomodoroProgress2 = new PomodoroProgress(pomodoroRoom, member2);
-//        pomodoroProgressRepository.save(pomodoroProgress1);
-//        pomodoroProgressRepository.save(pomodoroProgress2);
-//
-//        // when
-//        List<PomodoroProgress> pomodoroProgresses = pomodoroProgressRepository.findAllByPomodoroRoom(
-//                pomodoroRoom);
-//
-//        // then
-//        assertThat(pomodoroProgresses.size()).isEqualTo(2);
-//    }
-//
-//    @Test
-//    void room으로_pomodoroProgress와_member를_함께_조회한다() {
-//        // given
-//        Member member1 = new Member("member1");
-//        Member member2 = new Member("member2");
-//        ParticipantCode participantCode = new ParticipantCode(new CodeGenerationStrategy());
-//        PomodoroRoom pomodoroRoom = new PomodoroRoom("roomName", 1, 20, participantCode);
-//        memberRepository.save(member1);
-//        memberRepository.save(member2);
-//        participantCodeRepository.save(participantCode);
-//        pomodoroRoomRepository.save(pomodoroRoom);
-//
-//        PomodoroProgress pomodoroProgress1 = new PomodoroProgress(pomodoroRoom, member1);
-//        PomodoroProgress pomodoroProgress2 = new PomodoroProgress(pomodoroRoom, member2);
-//        pomodoroProgressRepository.save(pomodoroProgress1);
-//        pomodoroProgressRepository.save(pomodoroProgress2);
-//
-//        testEntityManager.flush();
-//        testEntityManager.clear();
-//
-//        PersistenceUtil persistenceUtil = testEntityManager.getEntityManager()
-//                .getEntityManagerFactory().getPersistenceUnitUtil();
-//
-//        // when
-//        List<PomodoroProgress> progresses = pomodoroProgressRepository.findAllByPomodoroRoomFetchMember(
-//                pomodoroRoom);
-//
-//        // then
-//        assertSoftly(softly -> {
-//            softly.assertThat(progresses).hasSize(2);
-//            softly.assertThat(persistenceUtil.isLoaded(member1)).isTrue();
-//            softly.assertThat(persistenceUtil.isLoaded(member2)).isTrue();
-//        });
-//    }
-//}
+package harustudy.backend.progress.repository;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.SoftAssertions.assertSoftly;
+
+import harustudy.backend.member.domain.LoginType;
+import harustudy.backend.member.domain.Member;
+import harustudy.backend.member.repository.MemberRepository;
+import harustudy.backend.room.domain.CodeGenerationStrategy;
+import harustudy.backend.room.domain.ParticipantCode;
+import harustudy.backend.room.repository.ParticipantCodeRepository;
+import harustudy.backend.progress.domain.PomodoroProgress;
+import harustudy.backend.room.domain.PomodoroRoom;
+import harustudy.backend.room.repository.PomodoroRoomRepository;
+import jakarta.persistence.PersistenceUtil;
+import java.util.List;
+import java.util.Optional;
+import org.junit.jupiter.api.DisplayNameGeneration;
+import org.junit.jupiter.api.DisplayNameGenerator.ReplaceUnderscores;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+
+@SuppressWarnings("NonAsciiCharacters")
+@DisplayNameGeneration(ReplaceUnderscores.class)
+@DataJpaTest
+class PomodoroProgressRepositoryTest {
+
+    @Autowired
+    private PomodoroProgressRepository pomodoroProgressRepository;
+
+    @Autowired
+    private PomodoroRoomRepository pomodoroRoomRepository;
+
+    @Autowired
+    private MemberRepository memberRepository;
+
+    @Autowired
+    private ParticipantCodeRepository participantCodeRepository;
+
+    @Autowired
+    private TestEntityManager testEntityManager;
+
+    @Test
+    void room과_member를_통해_pomodoroProgress를_조회한다() {
+        // given
+        Member member = new Member("member", "email", "imageUrl", LoginType.GUEST);
+        ParticipantCode participantCode = new ParticipantCode(new CodeGenerationStrategy());
+        PomodoroRoom pomodoroRoom = new PomodoroRoom("roomName", 1, 20, participantCode);
+        memberRepository.save(member);
+        participantCodeRepository.save(participantCode);
+        pomodoroRoomRepository.save(pomodoroRoom);
+
+        PomodoroProgress pomodoroProgress = new PomodoroProgress(pomodoroRoom, member, "nickname");
+        pomodoroProgressRepository.save(pomodoroProgress);
+
+        testEntityManager.flush();
+        testEntityManager.clear();
+
+        // when
+        Optional<PomodoroProgress> found = pomodoroProgressRepository.findByPomodoroRoomAndMember(
+                pomodoroRoom, member);
+
+        // then
+        assertThat(found).isPresent();
+        assertThat(found.get().getId()).isEqualTo(pomodoroProgress.getId());
+    }
+
+    @Test
+    void room으로_pomodoroProgress_리스트를_조회한다() {
+        // given
+        Member member1 = new Member("member1", "email", "imageUrl", LoginType.GUEST);
+        Member member2 = new Member("member2", "email", "imageUrl", LoginType.GUEST);
+        ParticipantCode participantCode = new ParticipantCode(new CodeGenerationStrategy());
+        PomodoroRoom pomodoroRoom = new PomodoroRoom("roomName", 1, 20, participantCode);
+        memberRepository.save(member1);
+        memberRepository.save(member2);
+        participantCodeRepository.save(participantCode);
+        pomodoroRoomRepository.save(pomodoroRoom);
+
+        PomodoroProgress pomodoroProgress1 = new PomodoroProgress(pomodoroRoom, member1, "nickname1");
+        PomodoroProgress pomodoroProgress2 = new PomodoroProgress(pomodoroRoom, member2, "nickname2");
+        pomodoroProgressRepository.save(pomodoroProgress1);
+        pomodoroProgressRepository.save(pomodoroProgress2);
+
+        // when
+        List<PomodoroProgress> pomodoroProgresses = pomodoroProgressRepository.findByPomodoroRoom(
+                pomodoroRoom);
+
+        // then
+        assertThat(pomodoroProgresses).hasSize(2);
+    }
+
+    @Test
+    void room으로_pomodoroProgress와_member를_함께_조회한다() {
+        // given
+        Member member1 = new Member("member1", "email", "imageUrl", LoginType.GUEST);
+        Member member2 = new Member("member2", "email", "imageUrl", LoginType.GUEST);
+        ParticipantCode participantCode = new ParticipantCode(new CodeGenerationStrategy());
+        PomodoroRoom pomodoroRoom = new PomodoroRoom("roomName", 1, 20, participantCode);
+        memberRepository.save(member1);
+        memberRepository.save(member2);
+        participantCodeRepository.save(participantCode);
+        pomodoroRoomRepository.save(pomodoroRoom);
+
+        PomodoroProgress pomodoroProgress1 = new PomodoroProgress(pomodoroRoom, member1, "nickname1");
+        PomodoroProgress pomodoroProgress2 = new PomodoroProgress(pomodoroRoom, member2, "nickname2");
+        pomodoroProgressRepository.save(pomodoroProgress1);
+        pomodoroProgressRepository.save(pomodoroProgress2);
+
+        testEntityManager.flush();
+        testEntityManager.clear();
+
+        PersistenceUtil persistenceUtil = testEntityManager.getEntityManager()
+                .getEntityManagerFactory().getPersistenceUnitUtil();
+
+        // when
+        List<PomodoroProgress> progresses = pomodoroProgressRepository.findAllByPomodoroRoomFetchMember(
+                pomodoroRoom);
+
+        // then
+        assertSoftly(softly -> {
+            softly.assertThat(progresses).hasSize(2);
+            softly.assertThat(persistenceUtil.isLoaded(member1)).isTrue();
+            softly.assertThat(persistenceUtil.isLoaded(member2)).isTrue();
+        });
+    }
+}
