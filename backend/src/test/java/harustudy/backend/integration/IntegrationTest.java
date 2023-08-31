07edaea5bdf31b0b15fbb48d938993a2a90e954d
Gyuseong Lee <lks971003@gmail.com>
2023-08-31 11:22:29 +0900

    [BE] Auth 관련 테스트를 추가한다 (#446)
    * refactor: 인증 헤더 파싱하는 중복 로직을 파서로 분리
    
    * test: auth 관련 테스트 추가
    
    * refactor: 메서드 분리 및 MockHttpServletResponse 사용
    
    * refactor: any()에 파라미터 타입 명시 및 개행 추가

--- backend/src/test/java/harustudy/backend/integration/IntegrationTest.java
+++ backend/src/test/java/harustudy/backend/integration/IntegrationTest.java
@@ -5,6 +5,7 @@ import static org.mockito.BDDMockito.given;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import harustudy.backend.auth.config.OauthProperty;
 import harustudy.backend.auth.config.TokenConfig;
 import harustudy.backend.auth.domain.RefreshToken;
 import harustudy.backend.auth.dto.OauthLoginRequest;
@@ -80,10 +81,10 @@ class IntegrationTest {
         OauthLoginRequest request = new OauthLoginRequest("google", "oauthLoginCode");
         String jsonRequest = objectMapper.writeValueAsString(request);
 
-        given(googleOauthClient.requestOauthToken(any(), any()))
+        given(googleOauthClient.requestOauthToken(any(String.class), any(OauthProperty.class)))
                 .willReturn(new OauthTokenResponse("mock-token-type", "mock-access-token",
                         "mock-scope"));
-        given(googleOauthClient.requestOauthUserInfo(any(), any()))
+        given(googleOauthClient.requestOauthUserInfo(any(OauthProperty.class), any(String.class)))
                 .willReturn(Map.of("name", name, "email", "mock-email", "picture", "mock-picture"));
 
         MvcResult result = mockMvc.perform(
