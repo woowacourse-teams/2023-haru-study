0bf66b239676de9df0b883c882cf9933cf7a81fd
teo <78679830+woosung1223@users.noreply.github.com>
2023-09-12 17:02:10 +0900

    [BE] progress 조회 시 없으면 null을 반환하는 임시 API 생성  (#490)
    * feat: progress 임시 조회용 API 생성
    
    * chore: 코드 스타일 수정
    
    * fix: progress 조회 결과가 빈 배열일 경우 null을 반환하도록 변경

--- backend/src/main/java/harustudy/backend/progress/controller/PomodoroProgressController.java
+++ backend/src/main/java/harustudy/backend/progress/controller/PomodoroProgressController.java
@@ -9,7 +9,6 @@ import harustudy.backend.progress.service.PomodoroProgressService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import java.net.URI;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -19,6 +18,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.net.URI;
 
 @Tag(name = "진행 관련 기능")
 @RequiredArgsConstructor
@@ -50,6 +51,19 @@ public class PomodoroProgressController {
                 pomodoroProgressService.findPomodoroProgressWithFilter(authMember, studyId, memberId);
         return ResponseEntity.ok(response);
     }
+
+    @Operation(summary = "필터링 조건으로 스터디 진행도 조회(임시)")
+    @GetMapping("/api/temp/studies/{studyId}/progresses")
+    public ResponseEntity<PomodoroProgressesResponse> findPomodoroProgressesWithFilterTemp(
+            @Authenticated AuthMember authMember,
+            @PathVariable Long studyId,
+            @RequestParam(required = false) Long memberId
+    ) {
+        PomodoroProgressesResponse response =
+                pomodoroProgressService.tempFindPomodoroProgressWithFilter(authMember, studyId, memberId);
+        return ResponseEntity.ok(response);
+    }
+
 
     @Operation(summary = "다음 스터디 단계로 이동")
     @ApiResponse(responseCode = "204")
