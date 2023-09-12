56272f0e27c886847f05ac830c6ff12c21050dbf
woo-jk <201702042@o.cnu.ac.kr>
2023-09-12 22:13:09 +0900

    feat: GlobalErrorBoundary, AlertErrorBoundary, ErrorFallback 구현 및 적용

---
+++ frontend/src/components/common/AlertErrorBoundary/AlertErrorBoundary.tsx
@@ -0,0 +1,35 @@
+import type { PropsWithChildren } from 'react';
+import { Component } from 'react';
+
+import type { ModalContextType } from '@Contexts/ModalProvider';
+import { ModalContext } from '@Contexts/ModalProvider';
+
+type AlertErrorBoundaryState = {
+  error: Error | null;
+};
+
+class AlertErrorBoundary extends Component<PropsWithChildren, AlertErrorBoundaryState> {
+  state: AlertErrorBoundaryState = {
+    error: null,
+  };
+
+  static getDerivedStateFromError(error: Error) {
+    return { error: error };
+  }
+
+  static contextType = ModalContext;
+
+  componentDidCatch(error: Error): void {
+    const { openAlert } = this.context as ModalContextType;
+
+    openAlert(error.message);
+  }
+
+  render() {
+    const { children } = this.props;
+
+    return children;
+  }
+}
+
+export default AlertErrorBoundary;
