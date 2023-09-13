15a4e320ee808a52975f8dcfb368889969efffd7
yeopto <gunyeop530@gmail.com>
2023-09-13 16:45:58 +0900

    refactor: custom error 수정

--- frontend/src/errors/index.ts
+++ frontend/src/errors/index.ts
@@ -1,23 +1,22 @@
-export class OfflineError extends Error {
-  constructor(message?: string) {
-    super(message || '인터넷 연결이 불안정 합니다. 네트워크 상태를 확인해주세요.');
-    this.name = 'Offline Error';
-  }
-}
-
 export class ApiError extends Error {
   code: number;
 
-  constructor(message: string, code: number) {
+  config: RequestInit;
+
+  constructor(message: string, code: number, config: RequestInit) {
     super(message);
     this.name = 'API Error';
     this.code = code;
+    this.config = config;
   }
 }
 
 export class UnknownApiError extends Error {
-  constructor() {
+  config: RequestInit;
+
+  constructor(config: RequestInit) {
     super('서버 요청에 문제가 발생했습니다. 잠시 후에 다시 시도해주세요.');
     this.name = 'Unknown API Error';
+    this.config = config;
   }
 }
