89816bbd0b4a10b795839070708e1fe3705f3f8d
woo-jk <201702042@o.cnu.ac.kr>
2023-09-20 11:18:39 +0900

    refactor: useStepTimer 주석 파일 상단으로 수정

--- frontend/src/components/progress/hooks/useStepTimer.ts
+++ frontend/src/components/progress/hooks/useStepTimer.ts
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import { useEffect } from 'react';
 
 import useTimer from '@Hooks/common/useTimer';
@@ -26,7 +27,6 @@ const useStepTimer = ({ studyMinutes, step, onStart, onStop, onComplete }: Param
     }
 
     start();
-    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [studyMinutes, step]);
 
   return {
