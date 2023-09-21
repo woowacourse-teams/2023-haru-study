1908e2f9d8235629fcca2dd6ebb1c3d22a87b67b
woo-jk <201702042@o.cnu.ac.kr>
2023-09-21 11:36:56 +0900

    feat: 학습 진행시 동적 파비콘 적용

---
+++ frontend/src/constants/asset.ts
@@ -0,0 +1,6 @@
+export const FAVICON_PATH = {
+  planning: '/favicon-blue.ico',
+  studying: '/favicon-red.ico',
+  retrospect: '/favicon-green.ico',
+  default: '/favicon.ico',
+} as const;
--- frontend/src/utils/dom.ts
+++ frontend/src/utils/dom.ts
@@ -2,6 +2,11 @@ const dom = {
   updateTitle: (title: string) => {
     document.title = title;
   },
+
+  updateFavicon: (path: string) => {
+    const favicon = document.getElementById('favicon') as HTMLLinkElement;
+    favicon.href = path;
+  },
 };
 
 export default dom;
