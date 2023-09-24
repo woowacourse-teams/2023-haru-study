9a30ff9aec65b087dc142f528ee004de7d0ea1ab
aak2075 <lks971003@gmail.com>
2023-09-25 00:15:24 +0900

    refactor: progress step을 waiting시 planning으로, done시 retrospect로 반환하도록 변경

--- backend/src/main/java/harustudy/backend/study/dto/StudyResponse.java
+++ backend/src/main/java/harustudy/backend/study/dto/StudyResponse.java
@@ -11,14 +11,32 @@ public record StudyResponse(Long studyId, String name, Integer totalCycle, Integ
     public static final String IN_PROGRESS = "inProgress";
 
     public static StudyResponse from(Study study) {
+        String roomStep = getRoomStep(study);
+        String progressStep = getProgressStep(study);
+
+        return new StudyResponse(study.getId(), study.getName(), study.getTotalCycle(), study.getTimePerCycle(),
+                study.getCurrentCycle(), roomStep, progressStep, study.getCreatedDate());
+    }
+
+    private static String getRoomStep(Study study) {
         String roomStep;
         if (study.isStep(Step.PLANNING) ||  study.isStep(Step.STUDYING) || study.isStep(Step.RETROSPECT)) {
             roomStep = IN_PROGRESS;
         } else {
             roomStep = study.getStep().name().toLowerCase();
         }
+        return roomStep;
+    }
 
-        return new StudyResponse(study.getId(), study.getName(), study.getTotalCycle(), study.getTimePerCycle(),
-                study.getCurrentCycle(), roomStep, study.getStep().name().toLowerCase(), study.getCreatedDate());
+    private static String getProgressStep(Study study) {
+        String progressStep;
+        if (study.isStep(Step.WAITING)) {
+            progressStep = Step.PLANNING.name().toLowerCase();
+        } else if (study.isStep(Step.DONE)) {
+            progressStep = Step.RETROSPECT.name().toLowerCase();
+        } else {
+            progressStep = study.getStep().name().toLowerCase();
+        }
+        return progressStep;
     }
 }
