c603df4c6ff23bb90a676b4fe4a9d9e6dae8a9cf
MoonJeWoong <munjin0201@naver.com>
2023-09-26 00:14:23 +0900

    feat: polling API 구현을 위한 view 패키지 및 기본 컨트롤러, 서비스 클래스 생성

--- backend/src/main/java/harustudy/backend/view/controller/PollingController.java
+++ backend/src/main/java/harustudy/backend/view/controller/PollingController.java
@@ -1,5 +1,10 @@
 package harustudy.backend.view.controller;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
 public class PollingController {
 
 }
--- backend/src/main/java/harustudy/backend/view/service/PollingService.java
+++ backend/src/main/java/harustudy/backend/view/service/PollingService.java
@@ -1,5 +1,12 @@
 package harustudy.backend.view.service;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@RequiredArgsConstructor
+@Transactional
+@Service
 public class PollingService {
 
 }
