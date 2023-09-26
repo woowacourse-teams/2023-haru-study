a8fd2743f4fa2738de0ae825c654a4dd71dacff7
hiiro <31722737+MoonJeWoong@users.noreply.github.com>
2023-09-26 18:05:00 +0900

    [BE] 진행 페이지 폴링 기능 구현하기 (#577)
    * feat: polling API 구현을 위한 view 패키지 및 기본 컨트롤러, 서비스 클래스 생성
    
    * feat: PollingController 구현
    
    * feat: 폴링 서비스 로직 및 테스트 구현
    
    * feat: 스터디 상태 값만 조회하도록 구현
    
    * feat: controller 단에 authMember 추가
    
    * feat: 기타 수정 사항 반영
    
    * feat: 스터디에서 step 조회 시 Enum으로 조회하도록 수정
    
    * feat: StudyRepository 메서드 네이밍 수정
    
    * style: 잘못된 개행 수정
    
    * fix: resolve merge conflict
    
    ---------
    
    Co-authored-by: aak2075 <lks971003@gmail.com>

--- backend/src/main/java/harustudy/backend/study/repository/StudyRepository.java
+++ backend/src/main/java/harustudy/backend/study/repository/StudyRepository.java
@@ -1,13 +1,20 @@
 package harustudy.backend.study.repository;
 
+import harustudy.backend.participant.domain.Step;
 import harustudy.backend.study.domain.Study;
 import harustudy.backend.study.exception.StudyNotFoundException;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface StudyRepository extends JpaRepository<Study, Long> {
-  
+
     default Study findByIdIfExists(Long id) {
         return findById(id)
                 .orElseThrow(StudyNotFoundException::new);
     }
+
+    @Query("select s.step from Study s where s.id = :id")
+    Optional<Step> findStepById(@Param("id") Long id);
 }
--- backend/src/main/java/harustudy/backend/view/service/PollingService.java
+++ backend/src/main/java/harustudy/backend/view/service/PollingService.java
@@ -1,12 +1,17 @@
 package harustudy.backend.view.service;
 
+import harustudy.backend.participant.domain.Step;
 import harustudy.backend.study.domain.Study;
+import harustudy.backend.study.exception.StudyNotFoundException;
 import harustudy.backend.study.repository.StudyRepository;
+import harustudy.backend.view.dto.ProgressResponse;
 import harustudy.backend.view.dto.WaitingResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @RequiredArgsConstructor
+@Transactional(readOnly = true)
 @Service
 public class PollingService {
 
@@ -15,5 +20,11 @@ public class PollingService {
     public WaitingResponse pollWaiting(Long studyId) {
         Study study = studyRepository.findByIdIfExists(studyId);
         return WaitingResponse.of(study, study.getParticipants());
+    }
+
+    public ProgressResponse pollProgress(Long studyId) {
+        Step step = studyRepository.findStepById(studyId)
+                .orElseThrow(StudyNotFoundException::new);
+        return ProgressResponse.from(step);
     }
 }
