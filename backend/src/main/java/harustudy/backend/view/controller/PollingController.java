eca1857b76669a0808a7ece98a3b42012a2244d5
woosung1223 <dntjd991223@naver.com>
2023-09-26 16:09:14 +0900

    refactor: Controller -> RestController로 변경

--- backend/src/main/java/harustudy/backend/view/controller/PollingController.java
+++ backend/src/main/java/harustudy/backend/view/controller/PollingController.java
@@ -7,12 +7,12 @@ import harustudy.backend.view.service.PollingService;
 import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @RequiredArgsConstructor
-@Controller
+@RestController
 public class PollingController {
 
     private final PollingService pollingService;
