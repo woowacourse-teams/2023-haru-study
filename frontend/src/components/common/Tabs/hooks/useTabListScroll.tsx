bc1bcae9c7b87c5b64265ba28b4bb0ac099eb183
nlom0218 <nlom0218@naver.com>
2023-09-26 17:22:55 +0900

    chore: 주석 제거

--- frontend/src/components/common/Tabs/hooks/useTabListScroll.tsx
+++ frontend/src/components/common/Tabs/hooks/useTabListScroll.tsx
@@ -26,16 +26,10 @@ const useTabListScroll = (tabList: RefObject<HTMLUListElement>) => {
   const handleScroll = useCallback(() => {
     const { scrollWidth, clientWidth, scrollLeft } = tabList.current!;
 
-    // scrollWidth: 스크롤까지 포함한 전체 너비
-    // clientWidth: 실제 화면에 보이는 너비
-    // scrollLeft: 가로로 스크롤한 너비
     const isEndOfList = Math.abs(scrollWidth - scrollLeft - clientWidth) < 1;
 
-    // 스크롤이 끝 지점으로 이동했을 때
     if (isEndOfList) setScrollButtonPosition('start');
-    // 스크롤이 앞 지점에 있을 때
     else if (scrollLeft === 0) setScrollButtonPosition('end');
-    // 양쪽에 스크롤이 생길 때
     else setScrollButtonPosition('both');
 
     if (!isTabListOverFlow) setScrollButtonPosition(null);
