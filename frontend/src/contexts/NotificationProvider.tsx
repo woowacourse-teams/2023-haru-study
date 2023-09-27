be2e940247706d27c38b9ea53a3602dc324476cd
nlom0218 <nlom0218@naver.com>
2023-09-27 13:50:33 +0900

    feat: NotificationContext 및 기본 구조 생성

---
+++ frontend/src/contexts/NotificationProvider.tsx
@@ -0,0 +1,54 @@
+import type { PropsWithChildren } from 'react';
+import { createContext, useContext, useState } from 'react';
+
+type Notification = {
+  variant: 'success' | 'error';
+  message: string;
+};
+
+export type NotificationContextType = {
+  send: (args: Notification) => void;
+};
+
+export const NotificationContext = createContext<NotificationContextType | null>(null);
+
+const NotificationProvider = ({ children }: PropsWithChildren) => {
+  const [notifications, setNotifications] = useState<Notification[] | null>(null);
+
+  const send = (args: Notification) =>
+    setNotifications((prev) => {
+      if (!prev) return [args];
+      return [...prev, args];
+    });
+
+  const value = {
+    send,
+  };
+
+  return (
+    <NotificationContext.Provider value={value}>
+      {children}
+      {notifications && <Notifications notifications={notifications} />}
+    </NotificationContext.Provider>
+  );
+};
+
+export default NotificationProvider;
+
+type Props = {
+  notifications: Notification[];
+};
+
+const Notifications = ({ notifications }: Props) => {
+  return (
+    <div>
+      {notifications.map((item, index) => (
+        <div key={index}>{item.message}</div>
+      ))}
+    </div>
+  );
+};
+
+export const useNotification = () => {
+  const value = useContext(NotificationContext);
+};
