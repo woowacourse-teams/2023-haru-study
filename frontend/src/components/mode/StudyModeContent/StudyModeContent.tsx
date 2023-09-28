c61b1b8c682d69919ea03809d70d36deaac8c76e
yeopto <gunyeop530@gmail.com>
2023-09-28 17:36:33 +0900

    feat: StudyMode 페이지 구현

---
+++ frontend/src/components/mode/StudyModeContent/StudyModeContent.tsx
@@ -0,0 +1,51 @@
+import { useNavigate } from 'react-router-dom';
+import { styled } from 'styled-components';
+
+import { ROUTES_PATH } from '@Constants/routes';
+
+import StudyModeItem from '../StudyModeItem/StudyModeItem';
+
+const StudyModeContent = () => {
+  const navigate = useNavigate();
+
+  const handleClickCreateStudyItem = () => {
+    navigate(`${ROUTES_PATH.create}?mode=together`);
+  };
+
+  const handleClickStudyParticipationItem = () => {
+    navigate(`${ROUTES_PATH.participation}`);
+  };
+
+  const handleClickSoloStudyItem = () => {
+    navigate(`${ROUTES_PATH.create}?mode=solo`);
+  };
+
+  return (
+    <Layout>
+      <StudyModeItem
+        title="스터디 개설하기"
+        description="스터디원들을 초대해서 함께 공부하고 싶어요"
+        onClick={handleClickCreateStudyItem}
+      />
+      <StudyModeItem
+        title="스터디 참여하기"
+        description="스터디장에게 받은 참여코드가 있어요"
+        onClick={handleClickStudyParticipationItem}
+      />
+      <StudyModeItem
+        title="혼자 학습하기"
+        description="다른 스터디원 없이 혼자 공부하고 싶어요"
+        onClick={handleClickSoloStudyItem}
+      />
+    </Layout>
+  );
+};
+
+export default StudyModeContent;
+
+const Layout = styled.div`
+  display: flex;
+  flex-direction: column;
+
+  gap: 30px;
+`;
--- frontend/src/router/index.tsx
+++ frontend/src/router/index.tsx
@@ -8,8 +8,9 @@ import { ROUTES_PATH } from '@Constants/routes';
 
 import App from '../App';
 
-const Auth = lazy(() => import(/* webpackChunkName: "Auth" */ '@Pages/Auth'));
+const Auth = lazy(() => import('@Pages/Auth'));
 const StudyRecord = lazy(() => import('@Pages/StudyRecord'));
+const StudyMode = lazy(() => import('@Pages/StudyMode'));
 const CreateStudy = lazy(() => import('@Pages/CreateStudy'));
 const MemberRecord = lazy(() => import('@Pages/MemberRecord'));
 const StudyProgress = lazy(() => import('@Pages/StudyProgress'));
@@ -53,6 +54,10 @@ const router = createBrowserRouter([
       {
         path: ROUTES_PATH.memberRecord,
         element: <MemberRecord />,
+      },
+      {
+        path: ROUTES_PATH.mode,
+        element: <StudyMode />,
       },
     ],
   },
