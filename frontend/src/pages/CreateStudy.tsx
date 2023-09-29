21ad20beba2eb350de7d5163a92099b284a3d504
Geonyeop Kim <gunyeop530@gmail.com>
2023-09-29 17:31:07 +0900

    [FE] 스터디 방식 페이지 구현 및 스터디 시작 플로우 수정 (#597)
    * refactor: LandingButton 로직 변경
    
    * test: UI 변경에 따른 테스트 코드 삭제
    
    * chore: ArrowIcon develop 브랜치 동기화
    
    * feat: StudyMode 페이지 구현
    
    * refactor: useMutation option 수정 및 onSuccess 수정
    
    * feat: 혼자하기 기능 추가
    
    * chore: 혼자 공부하기로 텍스트 변경
    
    * refactor: ArrowIcon으로 변경
    
    * chore: 개설 페이지 테스트 코드 삭제
    
    * chore: 혼자 공부하기에서 공부 시작하기로 텍스트 변경

--- frontend/src/pages/CreateStudy.tsx
+++ frontend/src/pages/CreateStudy.tsx
@@ -1,13 +1,22 @@
 import AlertErrorBoundary from '@Components/common/AlertErrorBoundary/AlertErrorBoundary';
 import CreateStudyForm from '@Components/create/CreateStudyForm/CreateStudyForm';
 
+import url from '@Utils/url';
+
+import type { StudyMode } from '@Types/study';
+
+import NotFound from './NotFound';
 import StudyParticipationLayout from './layout/StudyParticipationLayout';
 
 const CreateStudy = () => {
+  const studyMode: StudyMode = url.getQueryString('mode');
+
+  if (!(studyMode === 'together' || studyMode === 'solo')) return <NotFound />;
+
   return (
-    <StudyParticipationLayout headerText="스터디 개설하기">
+    <StudyParticipationLayout headerText={studyMode === 'solo' ? '공부 시작하기' : '스터디 개설하기'}>
       <AlertErrorBoundary>
-        <CreateStudyForm />
+        <CreateStudyForm studyMode={studyMode} />
       </AlertErrorBoundary>
     </StudyParticipationLayout>
   );
