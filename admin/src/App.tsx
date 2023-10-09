3cd5aad9a62fa74cdcd3022c3d8924fabcaddbb9
yeopto <gunyeop530@gmail.com>
2023-10-09 18:48:09 +0900

    style: reset css 추가

--- admin/src/App.tsx
+++ admin/src/App.tsx
@@ -1,7 +1,14 @@
 import { Outlet } from 'react-router-dom';
 
+import ResetStyles from '@Styles/reset';
+
 const App = () => {
-  return <Outlet />;
+  return (
+    <>
+      <ResetStyles />
+      <Outlet />
+    </>
+  );
 };
 
 export default App;
---
+++ admin/src/styles/reset.ts
@@ -0,0 +1,66 @@
+import { createGlobalStyle } from 'styled-components';
+
+const ResetStyles = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    font: inherit;
+    color: inherit;
+  }
+
+  *,
+  :after,
+  :before {
+    box-sizing: border-box;
+    flex-shrink: 0;
+  }
+
+  :root {
+    -webkit-tap-highlight-color: transparent;
+    -webkit-text-size-adjust: 100%;
+    text-size-adjust: 100%;
+    cursor: default;
+    line-height: 1.5;
+    overflow-wrap: break-word;
+    -moz-tab-size: 4;
+    tab-size: 4;
+  }
+
+  html,
+  body {
+    height: 100%;
+  }
+
+  img,
+  picture,
+  video,
+  canvas,
+  svg {
+    display: block;
+    max-width: 100%;
+  }
+
+  button {
+    background: none;
+    border: 0;
+    cursor: pointer;
+  }
+
+  a {
+    text-decoration: none;
+  }
+
+  table {
+    border-collapse: collapse;
+    border-spacing: 0;
+  }
+
+  ol,
+  ul {
+    list-style: none;
+    margin: 0px;
+    padding: 0px;
+  }
+`;
+
+export default ResetStyles;
