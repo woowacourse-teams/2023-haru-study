7a14db8dab308f50043b2a100cd9843b7dbfdb50
woo-jk <201702042@o.cnu.ac.kr>
2023-10-11 00:14:11 +0900

    feat: useStudyLobby 구현

---
+++ frontend/src/components/lobby/hooks/useStudyLobby.ts
@@ -0,0 +1,42 @@
+import useFetch from '@Hooks/api/useFetch';
+import useMutation from '@Hooks/api/useMutation';
+
+import {
+  requestDeleteParticipant,
+  requestGetLobbyInfo,
+  requestGetParticipant,
+  requestGetParticipantCode,
+  requestPostNextStep,
+} from '@Apis/index';
+
+const useStudyLobby = (studyId: string, memberId: string, onStartStudy: () => void) => {
+  const { result: participantCode } = useFetch(() => requestGetParticipantCode(studyId));
+  const { result: participantInfo } = useFetch(() => requestGetParticipant(studyId, memberId));
+  const { result: lobbyInfo } = useFetch(() => requestGetLobbyInfo(studyId), {
+    suspense: false,
+    refetchInterval: 2000,
+  });
+
+  const { mutate: startStudy, isLoading: isStarting } = useMutation(() => requestPostNextStep(studyId), {
+    onSuccess: onStartStudy,
+  });
+  const { mutate: exitStudy, isLoading: isExiting } = useMutation(() =>
+    requestDeleteParticipant(studyId, Number(participantInfo?.participantId)),
+  );
+
+  if (lobbyInfo && lobbyInfo.studyStep !== 'waiting') {
+    onStartStudy();
+  }
+
+  return {
+    participantCode: participantCode,
+    isHost: participantInfo?.isHost ?? false,
+    studyMembers: lobbyInfo?.participants ?? [],
+    startStudy: () => startStudy(),
+    isStarting,
+    exitStudy: () => exitStudy(),
+    isExiting,
+  };
+};
+
+export default useStudyLobby;
