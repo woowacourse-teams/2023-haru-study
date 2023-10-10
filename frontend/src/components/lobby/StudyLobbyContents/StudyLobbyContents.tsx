41b4ff9cede6d3a4831d2ccdcdb16eaee308de98
woo-jk <201702042@o.cnu.ac.kr>
2023-10-11 00:14:30 +0900

    feat: StudyLobbyContents 컴포넌트 구현

---
+++ frontend/src/components/lobby/StudyLobbyContents/StudyLobbyContents.tsx
@@ -0,0 +1,102 @@
+import { unstable_useBlocker, useNavigate, useParams } from 'react-router-dom';
+import { styled } from 'styled-components';
+
+import Button from '@Components/common/Button/Button';
+import ParticipantCodeCopier from '@Components/lobby/ParticipantCodeCopier/ParticipantCodeCopier';
+
+import { ROUTES_PATH } from '@Constants/routes';
+
+import { useMemberInfo } from '@Contexts/MemberInfoProvider';
+import { useNotification } from '@Contexts/NotificationProvider';
+
+import StudyMembers from '../StudyMembers/StudyMembers';
+import useStudyLobby from '../hooks/useStudyLobby';
+
+const StudyLobbyContents = () => {
+  const navigate = useNavigate();
+  const { send } = useNotification();
+
+  const { studyId } = useParams();
+  const memberInfo = useMemberInfo();
+
+  if (!studyId) throw new Error('잘못 된 접근입니다.');
+
+  const onStartStudy = () => {
+    send({ message: '스터디가 시작되었습니다.\n사이클의 목표를 설정해보세요.' });
+    navigate(`${ROUTES_PATH.progress}/${studyId}`);
+  };
+
+  const navigateToHome = () => {
+    navigate(ROUTES_PATH.landing);
+  };
+
+  const { isHost, participantCode, studyMembers, startStudy, isStarting, exitStudy, isExiting } = useStudyLobby(
+    studyId,
+    memberInfo!.memberId,
+    onStartStudy,
+  );
+
+  unstable_useBlocker(({ nextLocation }) => {
+    if (nextLocation.pathname === `${ROUTES_PATH.progress}/${studyId}`) return false;
+
+    if (confirm('정말로 스터디를 나가시겠습니까?')) {
+      exitStudy();
+      return false;
+    }
+
+    return true;
+  });
+
+  return (
+    participantCode && (
+      <Layout>
+        <ParticipantCodeCopier participantCode={participantCode}></ParticipantCodeCopier>
+        <StudyMembers studyMembers={studyMembers} />
+        <BottomButtonWrapper>
+          {isHost ? (
+            <>
+              <WaitingMessageWrapper>인원이 모두 참여했다면 시작 버튼을 눌러주세요.</WaitingMessageWrapper>
+              <Button variant="primary" onClick={startStudy} isLoading={isStarting}>
+                스터디 시작하기
+              </Button>
+              <Button variant="outlined" onClick={navigateToHome} isLoading={isExiting}>
+                스터디 나가기
+              </Button>
+            </>
+          ) : (
+            <>
+              <WaitingMessageWrapper>방장이 스터디를 시작하기 전까지 기다려주세요.</WaitingMessageWrapper>
+              <Button variant="danger" onClick={navigateToHome} isLoading={isExiting}>
+                스터디 나가기
+              </Button>
+            </>
+          )}
+        </BottomButtonWrapper>
+      </Layout>
+    )
+  );
+};
+
+export default StudyLobbyContents;
+
+const Layout = styled.section`
+  display: flex;
+  flex-direction: column;
+  gap: 70px;
+`;
+
+const BottomButtonWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 20px;
+`;
+
+const WaitingMessageWrapper = styled.div`
+  text-align: center;
+  font-size: 2rem;
+
+  @media screen and (max-width: 768px) {
+    font-size: 1.8rem;
+  }
+`;
