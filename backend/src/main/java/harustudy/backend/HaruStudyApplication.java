6a7f345cdc0441ebf28268c19c9fa770348e65d3
Gyuseong Lee <lks971003@gmail.com>
2023-10-16 14:52:04 +0900

    [BE] dev, prod 서버 시간 변경 (#662)
    * refactor: JVM 타임존 수정
    
    * refactor: 타임존 설정을 스프링에서 설정

--- backend/src/main/java/harustudy/backend/HaruStudyApplication.java
+++ backend/src/main/java/harustudy/backend/HaruStudyApplication.java
@@ -1,9 +1,12 @@
 package harustudy.backend;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableScheduling;
+
+import java.util.TimeZone;
 
 @EnableScheduling
 @EnableJpaAuditing
@@ -12,5 +15,10 @@ public class HaruStudyApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(HaruStudyApplication.class, args);
+    }
+
+    @PostConstruct
+    void started() {
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
     }
 }
