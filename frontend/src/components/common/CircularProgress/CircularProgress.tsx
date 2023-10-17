04e3df55ca1363616d8a12308685b845e4cf960b
nlom0218 <nlom0218@naver.com>
2023-10-17 16:36:24 +0900

    refactor: CircularProgress $circleColor props명 변경

--- frontend/src/components/common/CircularProgress/CircularProgress.tsx
+++ frontend/src/components/common/CircularProgress/CircularProgress.tsx
@@ -15,7 +15,7 @@ type Props = {
 
 const CircularProgress = ({ size = 'medium', circleColor = color.white, $style }: Props) => {
   return (
-    <StyledCircularProgress size={size} $style={$style} circleColor={circleColor}>
+    <StyledCircularProgress size={size} $style={$style} $circleColor={circleColor}>
       <div></div>
       <div></div>
       <div></div>
@@ -26,7 +26,13 @@ const CircularProgress = ({ size = 'medium', circleColor = color.white, $style }
 
 export default CircularProgress;
 
-const StyledCircularProgress = styled.div<Props>`
+type StyledCircularProgressProps = {
+  size?: Size;
+  $circleColor?: string;
+  $style?: CSSProp;
+};
+
+const StyledCircularProgress = styled.div<StyledCircularProgressProps>`
   display: flex;
   align-items: center;
   justify-content: center;
@@ -36,12 +42,12 @@ const StyledCircularProgress = styled.div<Props>`
     border-radius: 50%;
     animation: loading-ring 1.2s cubic-bezier(0.5, 0, 0.5, 1) infinite;
 
-    ${({ size = 'medium', circleColor, $style }) => css`
+    ${({ size = 'medium', $circleColor, $style }) => css`
       width: ${SIZE[size]};
       height: ${SIZE[size]};
 
-      border: 2px solid ${circleColor};
-      border-color: ${circleColor} transparent transparent transparent;
+      border: 2px solid ${$circleColor};
+      border-color: ${$circleColor} transparent transparent transparent;
 
       ${$style}
     `}
