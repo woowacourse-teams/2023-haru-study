592f1b30d1198da37f69abf9ea0a5af1b3574f27
nlom0218 <nlom0218@naver.com>
2023-10-18 14:47:10 +0900

    feat: useCacheMutation 생성

---
+++ frontend/src/hooks/api/useCacheMutation.ts
@@ -0,0 +1,66 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+import { useState } from 'react';
+
+import cacheStorage from '@Utils/CacheStorage';
+
+import useMutation from './useMutation';
+
+type Options<T> = {
+  queryKey: string[];
+  cacheTime?: number;
+  errorBoundary?: boolean;
+  onSuccess?: (result: T) => void | Promise<void>;
+  onError?: (error: Error) => void;
+};
+
+const useCacheMutation = <T>(
+  request: () => Promise<T>,
+  { errorBoundary = true, queryKey, cacheTime, onSuccess, onError }: Options<T>,
+) => {
+  const [isLoading, setIsLoading] = useState(false);
+  const [result, setResult] = useState<T | null>(null);
+  const { mutate, error } = useMutation(request, {
+    errorBoundary,
+    onSuccess,
+    onError,
+  });
+
+  const cacheData = cacheStorage.find<T>(queryKey);
+
+  const cacheMutate = async () => {
+    setIsLoading(true);
+
+    if (cacheData) {
+      await onSuccess?.(cacheData);
+      setResult(cacheData);
+
+      setIsLoading(false);
+      updateCacheData();
+
+      return;
+    }
+
+    const result = await mutate();
+
+    await onSuccess?.(result);
+    setResult(result);
+
+    cacheStorage.add<T>(queryKey || [], result, cacheTime);
+    setIsLoading(false);
+  };
+
+  const updateCacheData = async () => {
+    const result = await request();
+
+    if (cacheStorage.isEqualData(result, cacheData)) return;
+
+    await onSuccess?.(result);
+    setResult(result);
+
+    cacheStorage.add<T>(queryKey || [], result, cacheTime);
+  };
+
+  return { mutate: cacheMutate, result, isLoading, error };
+};
+
+export default useCacheMutation;
