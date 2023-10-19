a621bfe264a5eda3835e83da811373d28bad62e7
woosung1223 <dntjd991223@naver.com>
2023-10-19 20:07:00 +0900

    chore: 모든 API에 '/api' prefix 추가

--- backend/src/main/java/harustudy/backend/admin/controller/AdminController.java
+++ backend/src/main/java/harustudy/backend/admin/controller/AdminController.java
@@ -32,7 +32,7 @@ public class AdminController {
     private final AdminService adminService;
 
     @Operation(summary = "어드민 로그인")
-    @PostMapping(value = "/admin/login", consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
+    @PostMapping(value = "/api/admin/login", consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
     public ResponseEntity<Void> login(AdminLoginRequest request,
             HttpServletRequest httpServletRequest) {
         UUID sessionId = adminAuthService.login(request);
@@ -44,56 +44,56 @@ public class AdminController {
     }
 
     @Operation(summary = "멤버 조회")
-    @GetMapping("/admin/members")
+    @GetMapping("/api/admin/members")
     public ResponseEntity<AdminMembersResponse> findMembers(Pageable pageable, @RequestParam String loginType) {
         AdminMembersResponse response = adminService.findMembers(pageable, loginType);
         return ResponseEntity.ok(response);
     }
 
     @Operation(summary = "스터디 조회")
-    @GetMapping("/admin/studies")
+    @GetMapping("/api/admin/studies")
     public ResponseEntity<AdminStudiesResponse> findStudies(Pageable pageable) {
         AdminStudiesResponse response = adminService.findStudies(pageable);
         return ResponseEntity.ok(response);
     }
 
     @Operation(summary = "참여자 정보 조회")
-    @GetMapping("/admin/participants")
+    @GetMapping("/api/admin/participants")
     public ResponseEntity<AdminParticipantsResponse> findParticipants(Pageable pageable) {
         AdminParticipantsResponse response = adminService.findParticipants(pageable);
         return ResponseEntity.ok(response);
     }
 
     @Operation(summary = "콘텐츠 조회")
-    @GetMapping("/admin/contents")
+    @GetMapping("/api/admin/contents")
     public ResponseEntity<AdminContentsResponse> findContents(Pageable pageable) {
         AdminContentsResponse response = adminService.findContents(pageable);
         return ResponseEntity.ok(response);
     }
 
     @Operation(summary = "참여코드 조회")
-    @GetMapping("/admin/participant-codes")
+    @GetMapping("/api/admin/participant-codes")
     public ResponseEntity<AdminParticipantsResponse> findParticipantCodes(Pageable pageable) {
         AdminParticipantsResponse response = adminService.findParticipantCodes(pageable);
         return ResponseEntity.ok(response);
     }
 
     @Operation(summary = "오늘 생성된 스터디 조회")
-    @GetMapping("/admin/studies/created")
+    @GetMapping("/api/admin/studies/created")
     public ResponseEntity<AdminStudiesResponse> findStudiesCreatedToday(Pageable pageable) {
         AdminStudiesResponse response = adminService.findStudiesCreatedToday(pageable);
         return ResponseEntity.ok(response);
     }
 
     @Operation(summary = "오늘 완료된 스터디 조회")
-    @GetMapping("/admin/studies/done")
+    @GetMapping("/api/admin/studies/done")
     public ResponseEntity<AdminStudiesResponse> findStudiesDoneToday(Pageable pageable) {
         AdminStudiesResponse response = adminService.findStudiesDoneToday(pageable);
         return ResponseEntity.ok(response);
     }
 
     @Operation(summary = "스터디 정보 및 관련 컨텐츠 조회")
-    @GetMapping("/admin/studies/{studyId}/contents")
+    @GetMapping("/api/admin/studies/{studyId}/contents")
     public ResponseEntity<AdminStudyContentResponse> findContentsOfStudy(Pageable pageable,
             @PathVariable Long studyId) {
         AdminStudyContentResponse response = adminService.findContentsOfStudies(pageable, studyId);
