8f979bcee9692c4aa246028568f2088f6a1f000e
woosung1223 <dntjd991223@naver.com>
2023-11-06 19:38:29 +0900

    fix: 롬복 어노테이션 변경

--- backend/src/main/java/harustudy/backend/admin/entity/Admin.java
+++ backend/src/main/java/harustudy/backend/admin/entity/Admin.java
@@ -7,9 +7,10 @@ import jakarta.persistence.Id;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
-@AllArgsConstructor(access = AccessLevel.PROTECTED)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Entity
 public class Admin {
 
