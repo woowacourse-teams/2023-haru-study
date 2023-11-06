d3567490bf501868d85c74a1a51c10cc50b0526e
woosung1223 <dntjd991223@naver.com>
2023-11-06 19:33:34 +0900

    chore: `Admin` 엔티티 기본생성자 추가

--- backend/src/main/java/harustudy/backend/admin/entity/Admin.java
+++ backend/src/main/java/harustudy/backend/admin/entity/Admin.java
@@ -4,9 +4,12 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
+@AllArgsConstructor(access = AccessLevel.PROTECTED)
 @Entity
 public class Admin {
 
