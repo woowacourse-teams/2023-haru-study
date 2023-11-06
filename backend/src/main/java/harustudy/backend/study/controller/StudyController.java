af9ce7cf0f818c06865970e2149345178f73a4ae
Gyuseong Lee <lks971003@gmail.com>
2023-11-06 19:35:53 +0900

    [BE] 여러개의 쿼리파라미터를 ModelAttribute로 변경 (#719)
    * test: 멤버 아이디로 스터디 조회 테스트 추가
    
    * test: 참여 코드로 스터디 조회 테스트 추가
    
    * test: 멤버 아이디와 참여 코드로 스터디 조회 테스트 추가
    
    * refactor: 여러개의 쿼리 파라미터를 ModelAttribute로 수정
    
    * refactor: 라인 정렬

--- backend/src/main/java/harustudy/backend/study/controller/StudyController.java
+++ backend/src/main/java/harustudy/backend/study/controller/StudyController.java
@@ -4,20 +4,18 @@ import harustudy.backend.auth.Authenticated;
 import harustudy.backend.auth.dto.AuthMember;
 import harustudy.backend.study.dto.CreateStudyRequest;
 import harustudy.backend.study.dto.StudiesResponse;
+import harustudy.backend.study.dto.StudyFilterRequest;
 import harustudy.backend.study.dto.StudyResponse;
 import harustudy.backend.study.service.StudyService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
+
 import java.net.URI;
+
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @Tag(name = "스터디 관련 기능")
 @RequiredArgsConstructor
@@ -38,10 +36,9 @@ public class StudyController {
     @Operation(summary = "필터링 조건으로 스터디 조회")
     @GetMapping("/api/v2/studies")
     public ResponseEntity<StudiesResponse> findStudiesWithFilter(
-            @RequestParam(required = false) Long memberId,
-            @RequestParam(required = false) String participantCode
+            @ModelAttribute StudyFilterRequest request
     ) {
-        StudiesResponse response = studyService.findStudyWithFilter(memberId, participantCode);
+        StudiesResponse response = studyService.findStudyWithFilter(request);
         return ResponseEntity.ok(response);
     }
 
