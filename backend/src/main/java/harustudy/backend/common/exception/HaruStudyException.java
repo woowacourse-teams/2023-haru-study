1466390b225b25580a9a884863ebffa758d30ac8
MODI <77962265+jaehee329@users.noreply.github.com>
2023-11-21 11:17:35 +0900

    [BE] JWT 토큰 사용 제거, Interceptor와 ArgumentResolver 중복 로직 제거 #733
    * feat: jwt 대신 aes를 사용하도록 변경
    aes 기반 설정 정보 추가
    
    * refactor: Interceptor와 ArgumentResolver의 중복 로직 제거
    
    Interceptor에서 파싱한 memberId를 Request에 넣도록 변경
    
    * refactor: 예외 처리 위치 수정
    
    * chore: jwt 관련 의존성 build.gradle에서 제거
    
    * fix: submodule 관련 수정
    
    * refactor: parseSubject()의 예외 처리 로직 수정
    
    * fix: 누락된 예외 추가
    
    * refactor: 토큰 관련 로직을 별도의 클래스로 추상화
    
    * refactor: AccessTokenUtils 추가
    
    * fix: 잘못된 softAssertion 사용 수정

--- backend/src/main/java/harustudy/backend/common/exception/HaruStudyException.java
+++ backend/src/main/java/harustudy/backend/common/exception/HaruStudyException.java
@@ -5,6 +5,10 @@ public abstract class HaruStudyException extends RuntimeException {
     protected HaruStudyException() {
     }
 
+    protected HaruStudyException(Exception e) {
+        super(e);
+    }
+
     protected HaruStudyException(String message) {
         super(message);
     }
